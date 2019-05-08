559f4fbe6cf373b505cf7d4fee3b2e40645e6856
matt <mattramos@gmail.com>
2019-05-08 07:46:46 -0400

    Complete number validation

--- Intro To Programming/Array Methods/is_valid_email.rb
+++ Intro To Programming/Array Methods/is_valid_email.rb
@@ -22,7 +22,13 @@ def is_valid_email(str)
 end
 
 def contains_a_number(str)
-  # WIP
+  numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  str.each_char do |char|
+    if numbers.include?(char)
+      return true
+    end
+  end
+  return false
 end
 
 puts is_valid_email("abc@xy.z")         # => true
