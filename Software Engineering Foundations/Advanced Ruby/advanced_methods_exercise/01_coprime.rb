f1fef430b475fa988d1e44db488b27840ef05b94
Matt Ramos <mattramos@gmail.com>
2019-05-14 23:36:01 -0400

    Refactor coprime

--- Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/01_coprime.rb
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/01_coprime.rb
@@ -4,23 +4,7 @@
 # 1 is the only number that divides both 25 and 12.
 
 def coprime?(num1, num2)
-  all_divsors = get_divisors(num1) + get_divisors(num2)
-  !common_divisors?(all_divsors.sort)
-end
-
-def get_divisors(num)
-  divisors = []
-  (2..num).each do |i|
-    divisors << i if num % i == 0
-  end
-  divisors
-end
-
-def common_divisors?(arr)
-  arr.each_with_index do |i, idx|
-    return true if i == arr[idx + 1]
-  end
-  false
+  (2..num1).none? { |divisor| num1 % divisor == 0 && num2 % divisor == 0 }
 end
 
 p coprime?(25, 12)    # => true
