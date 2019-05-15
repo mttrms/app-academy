1f5b3a92334fbabc60d8b13802111d834f66574e
Matt Ramos <mattramos@gmail.com>
2019-05-15 07:39:38 -0400

    Bubble sort notes

---
+++ Software Engineering Foundations/Advanced Ruby/bubble_sort.rb
@@ -0,0 +1,23 @@
+# Bubble Sort
+# Bubble sort works by performing multiple passes to move elements closer to
+# their final positions. A single pass will iterate through the entire array once.
+
+arr = [5, 7, 1, 3, 2, 9]
+
+def bubble_sort(array)
+  sorted = false
+  while !sorted
+    sorted = true
+    
+    (0...array.length - 1).each do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+    end
+  end
+
+  array
+end
+
+print bubble_sort(arr)
