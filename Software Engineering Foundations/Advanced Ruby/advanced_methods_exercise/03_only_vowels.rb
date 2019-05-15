d6f973af05b83862a276d1a152ccaab27a123fab
Matt Ramos <mattramos@gmail.com>
2019-05-14 23:04:55 -0400

    Complete only vowels

--- Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/03_only_vowels.rb
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/03_only_vowels.rb
@@ -2,6 +2,16 @@
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
 
+def only_vowels?(str)
+  vowels = "aeiou"
+  str.each_char do |char|
+    if !vowels.include?(char)
+      return false
+    end
+  end
+  
+  true
+end
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
