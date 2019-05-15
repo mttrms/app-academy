d1dbfe10d100cd6b9f0f1e8f6136d8a7d9a9ccd2
Matt Ramos <mattramos@gmail.com>
2019-05-14 23:17:10 -0400

    Complete union

--- Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/06_union.rb
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/06_union.rb
@@ -1,6 +1,9 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 
+def union(*arr)
+  arr.flatten
+end
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
