e21c41b6e88a4527d61030b75dea1f227adee65f
Matt Ramos <mattramos@gmail.com>
2019-05-16 22:13:58 -0400

    Complete grid

--- Software Engineering Foundations/Scope and References/references_exercise/05_grid.rb
+++ Software Engineering Foundations/Scope and References/references_exercise/05_grid.rb
@@ -2,7 +2,9 @@
 # The method should return a 2D array with n rows and m columns (this means that there are n subarrays, each of length m).
 
 def grid(n, m)
+  grid = Array.new(n) {Array.new(m)}
 
+  grid
 end
 
 result_1 = grid(2, 3)
