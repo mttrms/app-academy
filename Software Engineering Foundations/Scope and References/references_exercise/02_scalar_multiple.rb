2fbd04f1a8b85e763e5bd959d1d237c3ddb7c3eb
Matt Ramos <mattramos@gmail.com>
2019-05-16 22:00:30 -0400

    Complete scalar multiple

--- Software Engineering Foundations/Scope and References/references_exercise/02_scalar_multiple.rb
+++ Software Engineering Foundations/Scope and References/references_exercise/02_scalar_multiple.rb
@@ -7,7 +7,11 @@
 # to be the same before and after calling your method.
 
 def scalar_multiple!(array, multiplier)
+  array.each_with_index do |num, idx|
+    array[idx] = num * multiplier
+  end
 
+  array
 end
 
 
