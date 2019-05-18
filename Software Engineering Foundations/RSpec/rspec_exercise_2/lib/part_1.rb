c6dda10bcb26980a7fe06a3801aa6cc877636fda
Matt Ramos <mattramos@gmail.com>
2019-05-18 14:30:19 -0400

    Complete merge in RSpec 2 Part 1

--- Software Engineering Foundations/RSpec/rspec_exercise_2/lib/part_1.rb
+++ Software Engineering Foundations/RSpec/rspec_exercise_2/lib/part_1.rb
@@ -8,3 +8,13 @@ def partition(arr, limit)
     [lower, higher]
   end
 end
+
+def merge(hash1, hash2)
+  merged_hash = hash2.clone # makes a 'shallow' copy of hash2
+
+  hash1.each do |k, v|
+    merged_hash[k] = v if !merged_hash.has_key?(k)
+  end
+
+  merged_hash
+end
