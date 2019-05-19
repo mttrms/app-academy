303520961c18806b486fda2b5d82fdd5d9813278
Matt Ramos <mattramos@gmail.com>
2019-05-19 11:17:30 -0400

    Clean up merge

--- Software Engineering Foundations/RSpec/rspec_exercise_2/lib/part_1.rb
+++ Software Engineering Foundations/RSpec/rspec_exercise_2/lib/part_1.rb
@@ -11,11 +11,7 @@ end
 
 def merge(hash1, hash2)
   merged_hash = hash2.clone # makes a 'shallow' copy of hash2
-
-  hash1.each do |k, v|
-    merged_hash[k] = v if !merged_hash.has_key?(k)
-  end
-
+  hash1.each { |k, v| merged_hash[k] = v if !merged_hash.has_key?(k) }
   merged_hash
 end
 
