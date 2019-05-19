999ca8d6c5113b9897bad5641d9e2bd28a0c05a9
Matt Ramos <mattramos@gmail.com>
2019-05-19 11:01:32 -0400

    Complete substrings

--- Software Engineering Foundations/RSpec/rspec_exercise_2/lib/part_2.rb
+++ Software Engineering Foundations/RSpec/rspec_exercise_2/lib/part_2.rb
@@ -3,3 +3,15 @@ def palindrome?(str)
   str.each_char { |char| reversed_str = char + reversed_str }
   reversed_str == str
 end
+
+def substrings(str)
+  subs = []
+
+  (0...str.length).each do |i|
+    (i...str.length).each do |j|
+      subs << str[i..j]
+    end
+  end
+
+  subs
+end
