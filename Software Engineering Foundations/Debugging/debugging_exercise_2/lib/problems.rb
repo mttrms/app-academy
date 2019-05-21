7c5aa4a437f26a3d3d05e275bd0a65cccb1e71b5
Matt Ramos <mattramos@gmail.com>
2019-05-20 21:31:32 -0400

    Complete largest prime factor

--- Software Engineering Foundations/Debugging/debugging_exercise_2/lib/problems.rb
+++ Software Engineering Foundations/Debugging/debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,23 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+require "byebug"
+
+def largest_prime_factor(num)
+  # debugger
+  divisors = (1..num).each.select {|i| num % i == 0 && is_prime?(i)}
+  divisors.max
+end
+
+def is_prime?(num)
+  return false if num < 2
+  (2...num).each do |factor|
+    return false if num % factor == 0
+  end
+    
+  true
+end
+
+# p largest_prime_factor(15)
+# p is_prime?(16)
