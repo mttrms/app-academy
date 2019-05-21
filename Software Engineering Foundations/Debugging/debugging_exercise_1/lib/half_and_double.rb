fc414d5b00e1ed25170374ca50f3e690c6b4709e
Matt Ramos <mattramos@gmail.com>
2019-05-20 21:05:55 -0400

    Complete half and double

--- Software Engineering Foundations/Debugging/debugging_exercise_1/lib/half_and_double.rb
+++ Software Engineering Foundations/Debugging/debugging_exercise_1/lib/half_and_double.rb
@@ -9,9 +9,9 @@ require "byebug"
 def half_and_double(array)
     new_array = array.map do |num|
         if num % 2 == 0
-            return num / 2
+            num / 2
         else
-            return num * 2
+            num * 2
         end
     end
 
