8d7fe56e89c2a9741a73ba402503712c77e0e1b1
Matt Ramos <mattramos@gmail.com>
2019-05-21 21:12:22 -0400

    Complete blocks part 1

---
+++ Software Engineering Foundations/Blocks & Procs/blocks_project/lib/part_1.rb
@@ -0,0 +1,21 @@
+def select_even_nums(arr)
+  arr.select(&:even?) # { |i| i.even? }
+end
+
+def reject_puppies(arr)
+  arr.reject { |dog| dog["age"] <= 2 }
+end
+
+def count_positive_subarrays(arr)
+  arr.count { |sub_array| sub_array.reduce(&:+) > 0 }
+end
+
+def aba_translate(str)
+  str.split("").map do |char|
+    "aeiou".include?(char) ? char + "b" + char : char
+  end.join("")
+end
+
+def aba_array(arr)
+  arr.map { |word| aba_translate(word) }
+end
