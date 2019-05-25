bd0375459a65f29d311d615cc4e4feabffec3918
matt <mattramos@gmail.com>
2019-05-25 12:32:58 -0400

    Complete dog project

--- Software Engineering Foundations/Classes/dog_project/lib/dog.rb
+++ Software Engineering Foundations/Classes/dog_project/lib/dog.rb
@@ -1,3 +1,38 @@
 class Dog
+def initialize(name, breed, age, bark, favorite_foods)
+  @name = name
+  @breed = breed
+  @age = age
+  @bark = bark
+  @favorite_foods = favorite_foods
+end
+
+def name
+  @name
+end
+
+def breed
+  @breed
+end
+
+def age
+  @age
+end
+
+def age=(num)
+  @age = num
+end
+
+def bark
+  @age > 3 ? @bark.upcase : @bark.downcase
+end
+
+def favorite_foods
+  @favorite_foods
+end
+
+def favorite_food?(str)
+  @favorite_foods.map(&:downcase).include?(str.downcase)
+end
 
 end
