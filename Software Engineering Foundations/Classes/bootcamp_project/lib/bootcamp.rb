5b1e11da7b71211e1a4a8f00e57b7a2a70b5f3ab
matt <mattramos@gmail.com>
2019-05-26 12:46:29 -0400

    Complete bootcamp project

--- Software Engineering Foundations/Classes/bootcamp_project/lib/bootcamp.rb
+++ Software Engineering Foundations/Classes/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,69 @@
 class Bootcamp
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |h,k| h[k] = [] }
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      true
+    else
+      false
+    end
+  end
   
+  def enrolled?(student)
+    @students.include?(student) ? true : false
+  end
+
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade(student, grade)
+    if enrolled?(student)
+      @grades[student] << grade
+      true
+    else
+      false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    if enrolled?(student) && @grades[student].length > 0
+      scores = @grades[student]
+      scores.sum / scores.length
+    else
+      nil
+    end
+  end
 end
