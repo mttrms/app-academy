b409f7a10a83a1bccda32807ff458f9f4f87d7f1
Matt Ramos <mattramos@gmail.com>
2019-05-29 22:32:29 -0400

    Complete hangman project

--- Software Engineering Foundations/Input & Output/hangman_project/lib/hangman.rb
+++ Software Engineering Foundations/Input & Output/hangman_project/lib/hangman.rb
@@ -30,6 +30,7 @@ class Hangman
 
   def get_matching_indices(guess)
     matches = []
+
     @secret_word.each_char.with_index do |char, idx|
       matches << idx if char == guess
     end
@@ -41,4 +42,58 @@ class Hangman
     indices.each { |idx| @guess_word[idx] = char }
   end
 
+  def try_guess(char)
+    if already_attempted?(char)
+      puts "that has already been attempted"
+      return false
+    end
+
+    # Log the char guessed
+    @attempted_chars << char
+
+    # Check for a match, decrement if wrong, fill if correct
+    indices = get_matching_indices(char)
+    if indices.length == 0
+      @remaining_incorrect_guesses -= 1 
+    else
+      fill_indices(char, indices)
+    end
+
+    # Return true because the guess was not previously attempted
+    true
+  end
+
+  def ask_user_for_guess
+    puts "Enter a char:"
+    guess = gets.chomp
+
+    try_guess(guess)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      puts "WIN"
+      return true
+    end
+
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts "LOSE"
+      return true
+    end
+
+    false
+  end
+
+  def game_over?
+    if win? || lose?
+      puts @secret_word
+      return true
+    end
+
+    false
+  end
 end
