8a2ceb817e364e0fe1ce40f3870fc04f9361982c
Matt Ramos <mattramos@gmail.com>
2019-05-29 22:57:22 -0400

    Complete hotel project part 1

--- Software Engineering Foundations/Input & Output/hotel_project/lib/room.rb
+++ Software Engineering Foundations/Input & Output/hotel_project/lib/room.rb
@@ -1,3 +1,32 @@
 class Room
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    @occupants.length == @capacity ? true : false
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    if self.full?
+      false
+    else
+      @occupants << name
+      true
+    end
+  end
 
 end
