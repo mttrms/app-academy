59523cbb4b2a73d53ba87dfbbf5a3e84a958cf7e
Matt Ramos <mattramos@gmail.com>
2019-06-01 10:56:25 -0400

    Complete mastermind part 1

--- Software Engineering Foundations/Object Oriented Programming/mastermind_project/lib/code.rb
+++ Software Engineering Foundations/Object Oriented Programming/mastermind_project/lib/code.rb
@@ -1,9 +1,42 @@
 class Code
+  attr_accessor :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
     "B" => :blue,
     "Y" => :yellow
   }
+
+  def self.valid_pegs?(pegs)
+    pegs.all? { |peg| POSSIBLE_PEGS.include?(peg.upcase) }
+  end
+
+  def self.random(num)
+    random_pegs = []
+
+    num.times do
+      random_pegs << POSSIBLE_PEGS.keys.sample
+    end
+
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(pegs)
+    Code.new(pegs.split(""))
+  end
+
+  def initialize(pegs)
+    @pegs = pegs.map(&:upcase)
+    raise "Pegs are invalid" if !Code.valid_pegs?(@pegs)
+  end
+
+  def [](idx)
+    self.pegs[idx]
+  end
+
+  def length
+    self.pegs.length
+  end
 
 end
