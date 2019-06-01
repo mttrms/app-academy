ec328f0e368f049419a8ee768f82cfe5f2e4812d
Matt Ramos <mattramos@gmail.com>
2019-06-01 13:24:09 -0400

    Complete mastermind project

--- Software Engineering Foundations/Object Oriented Programming/mastermind_project/lib/mastermind.rb
+++ Software Engineering Foundations/Object Oriented Programming/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,19 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    p "Number of exact matches: #{@secret_code.num_exact_matches(code)}"
+    p "Number of near matches: #{@secret_code.num_near_matches(code)}"
+  end
+
+  def ask_user_for_guess
+    p 'Enter a code'
+    guess = Code.from_string(gets.chomp)
+    print_matches(guess)
+    @secret_code == guess
+  end
 end
