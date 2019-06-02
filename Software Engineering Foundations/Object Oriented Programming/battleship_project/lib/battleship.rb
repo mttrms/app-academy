c24a17d4825b4ec7746d489f190eec249da29011
Matt Ramos <mattramos@gmail.com>
2019-06-01 20:22:50 -0400

    Complete battleship project

--- Software Engineering Foundations/Object Oriented Programming/battleship_project/lib/battleship.rb
+++ Software Engineering Foundations/Object Oriented Programming/battleship_project/lib/battleship.rb
@@ -2,5 +2,45 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_accessor :board, :player
+  
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = n * n / 2
+  end
 
+  def start_game
+    @board.place_random_ships
+    puts "#{@board.num_ships} ships on the board"
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts 'you lose'
+      true
+    else
+      false
+    end
+  end
+
+  def win?
+    if @board.num_ships == 0
+      puts 'you win'
+      true
+    else
+      false
+    end
+  end
+
+  def game_over?
+    lose? || win?
+  end
+
+  def turn
+    @remaining_misses -= 1 if !@board.attack(@player.get_move)
+    @board.print
+    puts "#{@remaining_misses} remaining attacks"
+  end
 end
