cf09b430e3fcaec1402d6b8103c83bc26b003072
Matt Ramos <mattramos@gmail.com>
2019-06-02 14:04:35 -0400

    Complete Hash#my_select

--- Software Engineering Foundations/Recap/recap_exercise_1/lib/problems.rb
+++ Software Engineering Foundations/Recap/recap_exercise_1/lib/problems.rb
@@ -71,22 +71,29 @@ def find_bigrams(str, bigrams)
 end
 
 class Hash
-    # Write a method, Hash#my_select, that takes in an optional proc argument
-    # The method should return a new hash containing the key-value pairs that return
-    # true when passed into the proc.
-    # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
-    #
-    # Examples:
-    #
-    # hash_1 = {x: 7, y: 1, z: 8}
-    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
-    #
-    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
-    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
-    # hash_2.my_select                            # => {4=>4}
-    def my_select(&prc)
+  # Write a method, Hash#my_select, that takes in an optional proc argument
+  # The method should return a new hash containing the key-value pairs that return
+  # true when passed into the proc.
+  # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
+  #
+  # Examples:
+  #
+  # hash_1 = {x: 7, y: 1, z: 8}
+  # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
+  #
+  # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
+  # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
+  # hash_2.my_select                            # => {4=>4}
+  def my_select(&prc)
+    prc ||= Proc.new { |k, v| k == v }
+    selected = {}
 
+    self.each do |k, v|
+      selected[k] = v if prc.call(k, v) == true
     end
+
+    selected
+  end
 end
 
 class String
