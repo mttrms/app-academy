2dbef7f0a5cce49ff23fbf2a37cd3104f24a6c77
Matt Ramos <mattramos@gmail.com>
2019-06-02 00:07:44 -0400

    complete pow, lucas number and sum array exercises

--- Software Engineering Foundations/Recursion/recursion_exercise/lib/problems.rb
+++ Software Engineering Foundations/Recursion/recursion_exercise/lib/problems.rb
@@ -12,7 +12,8 @@
 # pow(3, 4) # => 81
 # pow(4, 3) # => 64
 def pow(base, exponent)
-
+  return 1 if exponent == 0
+  base * pow(base, exponent - 1)
 end
 
 
@@ -35,7 +36,10 @@ end
 # lucas_number(5)   # =>    11
 # lucas_number(9)   # =>    76
 def lucas_number(n)
+  return 2 if n == 0
+  return 1 if n == 1
 
+  lucas_number(n - 1) + lucas_number(n - 2)
 end
 
 
@@ -51,7 +55,9 @@ end
 # sum_array([5, 2])         # => 7
 # sum_array([4, 10, -1, 2]) # => 15
 def sum_array(array)
+  return 0 if array.empty?
 
+  array.pop + sum_array(array)
 end
 
 
@@ -67,7 +73,7 @@ end
 # reverse_string("internet")    # => "tenretni"
 # reverse_string("friends")     # => "sdneirf"
 def reverse_string(str)
-
+  
 end
 
 
