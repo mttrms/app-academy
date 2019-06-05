d3e4be6322a8604a3d04da1fb083ec83190b7561
Matt Ramos <mattramos@gmail.com>
2019-06-04 20:52:40 -0400

    COmplete my_reverse

--- Ruby/Enumberables & Debugging/enumerables_project.rb
+++ Ruby/Enumberables & Debugging/enumerables_project.rb
@@ -101,6 +101,16 @@ class Array
 
     joined
   end
+
+  def my_reverse
+    rotated = []
+    
+    self.each do |ele|
+      rotated.unshift(ele)
+    end
+
+    rotated
+  end
 end
 
 puts "My Each"
@@ -188,3 +198,10 @@ puts "My Join"
 a = [ "a", "b", "c", "d" ]
 p a.my_join         # => "abcd"
 p a.my_join("$")    # => "a$b$c$d"
+
+puts "My Reverse"
+# Write a method that returns a new array containing all the elements of the
+# original array in reverse order.
+
+p [ "a", "b", "c" ].my_reverse   #=> ["c", "b", "a"]
+p [ 1 ].my_reverse               #=> [1]
