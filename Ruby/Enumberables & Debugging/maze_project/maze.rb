dec96882e00d806b20f4289deb10a322fadb78b9
mttrms <matt.ramos@policygenius.com>
2019-06-05 13:11:33 -0400

    Parse mazes

--- Ruby/Enumberables & Debugging/maze_project/maze.rb
+++ Ruby/Enumberables & Debugging/maze_project/maze.rb
@@ -1,4 +1,29 @@
 class Maze
-    def initialize
+    def initialize(puzzle)
+        @puzzle = []
+        File.open(File.dirname(__FILE__) + "/#{puzzle}").each do |line|
+            
+            parsed_line = []
+            line.each_char do |char|
+                puts char
+
+                if char == "*"
+                    parsed_line << "#"
+                elsif char == " "
+                    parsed_line << "."
+                elsif char == "S" || char == "E"
+                    parsed_line << char
+                end
+
+            end
+            @puzzle << parsed_line
+            
+        end
+        @puzzle.each do |line|
+            p line
+        end
     end
+
 end
+
+test = Maze.new("maze_01.txt")
