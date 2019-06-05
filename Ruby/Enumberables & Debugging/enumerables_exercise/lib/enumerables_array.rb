6a9fc0826733f063cd493b8e895035d4b1f355db
Matt Ramos <mattramos@gmail.com>
2019-06-04 21:26:14 -0400

    Complete foundations review

--- Ruby/Enumberables & Debugging/enumerables_exercise/lib/enumerables_array.rb
+++ Ruby/Enumberables & Debugging/enumerables_exercise/lib/enumerables_array.rb
@@ -41,3 +41,70 @@ def subwords(word, dictionary)
   subs = substrings(word)
   subs.select { |sub| dictionary.include?(sub) }.uniq
 end
+
+# ### Bubble Sort
+#
+# http://en.wikipedia.org/wiki/bubble_sort
+#
+# Implement Bubble sort in a method, `Array#bubble_sort!`. Your method should
+# modify the array so that it is in sorted order.
+#
+# > Bubble sort, sometimes incorrectly referred to as sinking sort, is a
+# > simple sorting algorithm that works by repeatedly stepping through
+# > the list to be sorted, comparing each pair of adjacent items and
+# > swapping them if they are in the wrong order. The pass through the
+# > list is repeated until no swaps are needed, which indicates that the
+# > list is sorted. The algorithm gets its name from the way smaller
+# > elements "bubble" to the top of the list. Because it only uses
+# > comparisons to operate on elements, it is a comparison
+# > sort. Although the algorithm is simple, most other algorithms are
+# > more efficient for sorting large lists.
+#
+# Hint: Ruby has parallel assignment for easily swapping values:
+# http://rubyquicktips.com/post/384502538/easily-swap-two-variables-values
+#
+# After writing `bubble_sort!`, write a `bubble_sort` that does the same
+# but doesn't modify the original. Do this in two lines using `dup`.
+#
+# Finally, modify your `Array#bubble_sort!` method so that, instead of
+# using `>` and `<` to compare elements, it takes a block to perform the
+# comparison:
+#
+# ```ruby
+# [1, 3, 5].bubble_sort! { |num1, num2| num1 <=> num2 } #sort ascending
+# [1, 3, 5].bubble_sort! { |num1, num2| num2 <=> num1 } #sort descending
+# ```
+#
+# #### `#<=>` (the **spaceship** method) compares objects. `x.<=>(y)` returns
+# `-1` if `x` is less than `y`. If `x` and `y` are equal, it returns `0`. If
+# greater, `1`. For future reference, you can define `<=>` on your own classes.
+#
+# http://stackoverflow.com/questions/827649/what-is-the-ruby-spaceship-operator
+
+class Array
+  def bubble_sort!(&prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+    sorted = false
+    
+    while !sorted
+      sorted = true
+
+      (0...self.length - 1).each do |i|
+        if prc.call(self[i], self[i + 1]) == 1
+          sorted = false
+          self[i], self[i + 1] = self[i + 1], self[i]
+        end
+      end
+    end
+
+    self
+  end
+
+  def bubble_sort(&prc)
+    duped = self.dup
+    duped.bubble_sort!
+  end
+end
+
+
+# p [5,1,2,6,7].bubble_sort! { |a, b| a <=> b }
