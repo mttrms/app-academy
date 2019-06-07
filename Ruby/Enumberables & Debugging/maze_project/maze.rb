f812c0f9410793d39b98fb1f1416cbc0789558d1
Matt Ramos <mattramos@gmail.com>
2019-06-06 20:51:59 -0400

    Clean up maze solver

--- Ruby/Enumberables & Debugging/maze_project/maze.rb
+++ Ruby/Enumberables & Debugging/maze_project/maze.rb
@@ -3,29 +3,17 @@ require 'set'
 class Maze
   def initialize(maze_name)
       @simple_puzzle = generate_maze(maze_name)
-      # @simple_puzzle = [
-      #   ["#",".",".",".",".",".",".","E"],
-      #   ["#",".",".",".",".",".","S","."]
-      # ]
       @current_position = find_start(@simple_puzzle)
-      @end_position = find_end(@simple_puzzle)
-    
       @traversed = Set.new
-      
 
-      # print_maze(@simple_puzzle)
       make_move(@current_position)
-      # p @simple_puzzle[2][1]
   end
 
   def generate_maze(puzzle)
       @maze = []
-      File.open(File.dirname(__FILE__) + "/#{puzzle}").each do |line|
-          
+      File.open(File.dirname(__FILE__) + "/#{puzzle}").each do |line|    
           parsed_line = []
           line.each_char do |char|
-              # puts char
-
               if char == "*"
                   parsed_line << "#"
               elsif char == " "
@@ -33,10 +21,8 @@ class Maze
               elsif char == "S" || char == "E"
                   parsed_line << char
               end
-
           end
           @maze << parsed_line
-          
       end
 
       @maze
@@ -57,18 +43,7 @@ class Maze
     end
   end
 
-  def find_end(maze)
-    maze.each_with_index do |row, idx|
-      if row.include?("E")
-      end_pos = [idx, row.index("E")]
-        return end_pos
-      end
-    end
-  end
-
   def make_move(current_position)
-    
-    
     x = current_position[1]
     y = current_position[0]
     
@@ -85,41 +60,27 @@ class Maze
       return false
     end
 
-    
-
     @traversed << current_position
 
     # try left
     if make_move([y, x - 1]) 
-      p current_position
       return true
     end
 
     # try up
     if make_move([y - 1, x])
-      p current_position
       return true
     end
 
     # try right
     if make_move([y, x + 1])
-      p current_position
       return true
     end
 
-    # try down
     if make_move([y + 1, x])
-      p current_position
       return true
     end
-    
-    #   make_move([y, x - 1])
-    # el  sif @simple_puzzle[y - 1][x]
-    #   puts "up"
-    # end
-
   end
-
 end
 
-test = Maze.new("maze_01.txt")
+ Maze.new("maze_01.txt")
