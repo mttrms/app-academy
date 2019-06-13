604cea687425590cf5901cfa8c2dfe3c6c90f076
Matt Ramos <mattramos@gmail.com>
2019-06-12 23:00:27 -0400

    Add validation to prompt

--- Ruby/Reference/sudoku/sudoku.rb
+++ Ruby/Reference/sudoku/sudoku.rb
@@ -23,18 +23,37 @@ class Sudoku
   end
 
   def prompt
-    puts "What tile would you like to change? Example: 0,2"
-    @pos = gets.chomp
-    puts "To what value?"
-    @value = gets.chomp
+    until valid_pos?(@pos)
+      puts "What tile would you like to change? Example: 0,2"
+      @pos = gets.chomp
+    end
+
+    until valid_val?(@value)
+      puts "To what value?"
+      @value = gets.chomp
+    end
   end
 
   def fill_in(pos, val)
     position = pos.split(",").map!(&:to_i)
     @board[position].value = val.to_i
+    @pos = nil
+    @value = nil
+  end
+
+  def valid_pos?(pos)
+    return false if pos == nil
+
+    position = pos.split(",").map!(&:to_i)
+    @board[position].given == false
+  end
+
+  def valid_val?(val)
+    return false if val == nil || !(1..8).to_a.include?(val.to_i)
+    
+    true
   end
 
 end
 
 game = Sudoku.new("sudoku2.txt")
-# game.play
