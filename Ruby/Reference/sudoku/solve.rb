b6c779716ba0c22cb9278fc04df6894ab6995371
Matt Ramos <mattramos@gmail.com>
2019-06-13 23:01:42 -0400

    Find next unsolved cell

--- Ruby/Reference/sudoku/solve.rb
+++ Ruby/Reference/sudoku/solve.rb
@@ -5,10 +5,15 @@ class Solve
   def initialize(filename)
     @board = Board.from_file(filename)
     @grid = Board.build_grid(@board)
+    @board.render
   end
 
-  def find_cell(board)
-    
+  def find_cell(grid, cell)
+    (0..8).each do |row_idx|
+      (0..8).each do |col_idx|
+        return [row_idx, col_idx] if grid[row_idx][col_idx] == 0
+      end
+    end
   end
 
 end
