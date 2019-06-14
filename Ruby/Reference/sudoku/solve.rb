1e599e16e5b399d576f95b9826c90d92530019c5
Matt Ramos <mattramos@gmail.com>
2019-06-13 23:05:01 -0400

    Update find cell method

--- Ruby/Reference/sudoku/solve.rb
+++ Ruby/Reference/sudoku/solve.rb
@@ -6,16 +6,25 @@ class Solve
     @board = Board.from_file(filename)
     @grid = Board.build_grid(@board)
     @board.render
+    cell = [0,0]
+    find_cell(@grid, cell)
+    p cell
   end
 
   def find_cell(grid, cell)
     (0..8).each do |row_idx|
       (0..8).each do |col_idx|
-        return [row_idx, col_idx] if grid[row_idx][col_idx] == 0
+        if grid[row_idx][col_idx] == 0
+          cell[0] = row_idx
+          cell[1] = col_idx
+          return true
+        end
       end
     end
+
+    false
   end
 
 end
 
-my_solve = Solve.new("sudoku1.txt")
+my_solve = Solve.new("sudoku2.txt")
