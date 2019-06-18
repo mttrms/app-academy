05a2361894c02b8f4189c2004c2520dbe3d9f75d
mttrms <matt.ramos@policygenius.com>
2019-06-18 11:13:05 -0400

    Refactor binary search

--- Ruby/Recursion/recursion_exercises/binary_search.rb
+++ Ruby/Recursion/recursion_exercises/binary_search.rb
@@ -1,34 +1,23 @@
-require "byebug"
-
-# def bsearch(arr, target)
-#   return target if arr.first == target
-#   return nil if arr.length == 1 && arr.first != target
-#   mid_point = arr.length / 2
-#   # p target < arr[(arr.length / 2)]
-
-  # target < arr[(arr.length / 2)] ? bsearch(arr[0...mid_point], target) : bsearch(arr[mid_point..-1], target)
-
+def bsearch(arr, target, left = 0, right = nil)
+  right = arr.length - 1 if right == nil
   
-# end
+  if right >= left
+    mid = left + (right - left) / 2
 
-# def bsearch(arr, target, left = 0, right = nil)
-#   right = (arr.length - 1) if right == nil
-#   mid = (right + left) / 2
-  
+    if arr[mid] == target
+      return mid
+    elsif target < arr[mid]
+      bsearch(arr, target, 0, mid - 1)
+    else
+      bsearch(arr, target, mid + 1, right)
+    end
+    
+  else
+    nil
+  end
+end
 
-#   return mid if arr[mid] == target
-
-#   if target < arr[mid]
-#     bsearch(arr[0..mid], target, left, mid)
-#   else 
-#     bsearch(arr[mid + 1..right], target, mid + 1, right)
-#   end
-  
-  
-
-# end
-
-def bsearch(arr, target)
+def bsearch_alt(arr, target)
   mid_point = arr.length / 2
   lower = arr[0...mid_point]
   upper = arr[mid_point..-1]
@@ -40,8 +29,6 @@ def bsearch(arr, target)
   elsif arr[mid_point] < target
     bsearch(upper, target) + mid_point
   end
-
-  # lower
 end
 
 # Test Cases
@@ -50,5 +37,5 @@ p bsearch([2, 3, 4, 5], 3) # => 1
 p bsearch([2, 4, 6, 8, 10], 6) # => 2
 p bsearch([1, 3, 4, 5, 9], 5) # => 3
 p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
