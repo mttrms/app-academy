6bd3b709a539104e8f6d5b271e6c9f5815dde30e
Matt Ramos <mattramos@gmail.com>
2019-06-19 23:55:58 -0400

    Cleanup mergesort

--- Ruby/Recursion/recursion_exercises/merge_sort.rb
+++ Ruby/Recursion/recursion_exercises/merge_sort.rb
@@ -1,7 +1,5 @@
-require "byebug"
-
 def merge_sort(arr)
-  return [arr.first] if arr.length == 1
+  return arr if arr.length == 1
   mid = (arr.length - 1) / 2
   left = merge_sort(arr[0..mid])
   right = merge_sort(arr[mid + 1..-1])
