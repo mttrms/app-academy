3db07f9ad4893c30108f435fabede95a2b7af7a3
Matt Ramos <mattramos@gmail.com>
2019-06-19 23:53:03 -0400

    Create merge method to complete mergesort functionality

--- Ruby/Recursion/recursion_exercises/merge_sort.rb
+++ Ruby/Recursion/recursion_exercises/merge_sort.rb
@@ -1,13 +1,37 @@
 require "byebug"
 
 def merge_sort(arr)
-  debugger
-  return arr.first if arr.length == 1
+  return [arr.first] if arr.length == 1
   mid = (arr.length - 1) / 2
-  
-  merge_sort(arr[0..mid])
-  merge_sort(arr[mid + 1..-1])  
+  left = merge_sort(arr[0..mid])
+  right = merge_sort(arr[mid + 1..-1])
+
+  merge(left, right)
+end
+
+def merge(l, r)
+  merged = []
+
+  while l.any? && r.any?
+    if l[0] < r[0]
+      merged << l.shift
+    else
+      merged << r.shift
+    end
+  end
+
+  while l.any?
+    merged << l.shift
+  end
+
+  while r.any?
+    merged << r.shift
+  end
+
+  merged
 end
 
 # Test cases
 p merge_sort([6, 5, 3, 1, 8, 7, 2, 4])
+p merge_sort(["x", "z", "a", "c", "b", "v", "d", "k"])
+p merge_sort([9,8,7,6,5,6,7,8,9,0,9,8,7,6,5,4,3,2,3,2,3,2,3,2])
