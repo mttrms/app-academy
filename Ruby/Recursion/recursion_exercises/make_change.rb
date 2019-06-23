2998bb95f14bc3d07595d35fcb36a7cb9d5210cf
Matt Ramos <mattramos@gmail.com>
2019-06-22 23:00:36 -0400

    Iterative greedy make change

--- Ruby/Recursion/recursion_exercises/make_change.rb
+++ Ruby/Recursion/recursion_exercises/make_change.rb
@@ -1,5 +1,20 @@
-def greedy_make_change(amount, coins = [25, 10, 5, 1])
+def greedy_make_change_iterative(amount, coins = [25, 10, 5, 1])
+  change = []
+  
+  until change.sum == amount
+    coins.each do |coin|
+      if (amount - change.sum) >= coin
+        change << coin
+        next
+      end
+    end
+  end
 
+  change
+  
+end
+
+def greedy_make_change(amount, coins = [25, 10, 5, 1])
 end
 
 # Test Cases
