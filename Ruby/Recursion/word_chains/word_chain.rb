844d7d295f487cdeabaaed58fb63c2c07b23ecb6
Matt Ramos <mattramos@gmail.com>
2019-06-23 19:51:02 -0400

    Working adjacent_words method

--- Ruby/Recursion/word_chains/word_chain.rb
+++ Ruby/Recursion/word_chains/word_chain.rb
@@ -3,7 +3,7 @@ require 'set'
 class WordChainer
   def initialize(dictionary_filename)
     @dictionary = create_dictionary(dictionary_filename)
-    adjacent_words("cat")
+    p adjacent_words("cat")
   end
 
   def create_dictionary(filename)
@@ -18,6 +18,7 @@ class WordChainer
 
   def adjacent_words(word)
     same_size_words = []
+    adjacent_words = []
 
     @dictionary.each do |dict_word|
       if word.length == dict_word.length
@@ -25,7 +26,15 @@ class WordChainer
       end
     end
 
-    same_size_words
+    same_size_words.each do |dict_word|
+      (0...dict_word.length).each do |i|
+        dict_frag = dict_word[0...i] + dict_word[i + 1..-1]
+        word_frag = word[0...i] + word[i + 1..-1]
+        adjacent_words << dict_word if dict_frag == word_frag && dict_word != word
+      end
+    end
+
+    adjacent_words
   end
 end
 
