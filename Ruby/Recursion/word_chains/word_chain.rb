6f5ec755fcca1a59ddcc1c65c7dfbd7a390cf0ad
Matt Ramos <mattramos@gmail.com>
2019-06-23 19:19:47 -0400

    Create dictionary

--- Ruby/Recursion/word_chains/word_chain.rb
+++ Ruby/Recursion/word_chains/word_chain.rb
@@ -1,5 +1,17 @@
+require 'set'
+
 class WordChainer
-  def initialize
-    
+  def initialize(dictionary_filename)
+    @dictionary = create_dictionary(dictionary_filename)
+  end
+
+  def create_dictionary(filename)
+    dictionary = Set.new
+
+    File.foreach(File.dirname(__FILE__) + '/' + filename) do |word|
+      dictionary << word.chomp
+    end
+
+    dictionary
   end
 end
