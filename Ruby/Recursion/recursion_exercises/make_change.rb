7f6245e6cd3ab6fbdccf2c22c97b556cb3d26972
Matt Ramos <mattramos@gmail.com>
2019-06-23 02:17:35 -0400

    Complete make_change

--- Ruby/Recursion/recursion_exercises/make_change.rb
+++ Ruby/Recursion/recursion_exercises/make_change.rb
@@ -1,20 +1,5 @@
 require "byebug"
 
-def greedy_make_change_iterative(amount, coins = [25, 10, 5, 1])
-  change = []
-  
-  until change.sum == amount
-    coins.each do |coin|
-      if (amount - change.sum) >= coin
-        change << coin
-        break
-      end
-    end
-  end
-
-  change
-end
-
 def greedy_make_change(amount, coins = [25, 10, 5, 1])
   return [] if amount == 0
 
@@ -31,21 +16,24 @@ end
 
 def make_better_change(amount, coins = [25, 10, 5, 1])
   return [] if amount == 0
-  change = []
 
-  coins.each_with_index do |coin, idx|
-    if coin <= amount - change.sum
-      change << coin
-      next_coin = make_better_change(amount - coin, coins[idx..-1])
-      change += next_coin
+  best_change = nil
+  coins.each do |coin|
+    next if coin > amount
+
+    change = []
+    change << coin
+    change += make_better_change(amount - coin, coins)
+
+    if best_change == nil || change.length < best_change.length
+      best_change = change
     end
   end
 
-  change
+  best_change
 end
 
 # Test Cases
-# p greedy_make_change(39) # => [25, 10, 1, 1, 1, 1]
-# p greedy_make_change(24, [10,7,1]) # => [10, 10, 1, 1, 1, 1] but should be.... [10, 7, 7]
-# p greedy_make_change_iterative(24, [10, 7, 1]) # => [25, 10, 1, 1, 1, 1]
+p greedy_make_change(39) # => [25, 10, 1, 1, 1, 1]
+p greedy_make_change(24, [10, 7, 1]) # => [10, 10, 1, 1, 1, 1] but should be.... [10, 7, 7]
 p make_better_change(24, [10, 7, 1]) # => [10, 7, 7]
