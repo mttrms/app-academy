369b62362807e2921fa7c171764ed43851893e02
Matt Ramos <mattramos@gmail.com>
2019-06-24 19:05:00 -0400

    Find all word combinations with #run

--- Ruby/Recursion/word_chains/word_chain.rb
+++ Ruby/Recursion/word_chains/word_chain.rb
@@ -3,7 +3,7 @@ require 'set'
 class WordChainer
   def initialize(dictionary_filename)
     @dictionary = create_dictionary(dictionary_filename)
-    p adjacent_words("cat")
+    p run("test", "word")
   end
 
   def create_dictionary(filename)
@@ -16,10 +16,31 @@ class WordChainer
     dictionary
   end
 
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = [source]
+
+    while @current_words.any?
+      new_current_words = []
+      @current_words.each do |word|
+        adjacent_words(word).each do |adjacent_word|
+          next if @all_seen_words.include?(adjacent_word)
+
+          new_current_words << adjacent_word
+          @all_seen_words << adjacent_word
+        end
+
+        @current_words = new_current_words
+      end
+    end
+
+    @all_seen_words
+  end
+
   def adjacent_words(word)
     adjacent_words = []
     @dictionary.each do |dict_word|
-      if word.length == dict_word.length && is_adjacent?(word, dict_word) && word != dict_word
+      if word.length == dict_word.length && is_adjacent?(word, dict_word)
         adjacent_words << dict_word
       end
     end
@@ -31,7 +52,7 @@ class WordChainer
     (0...test_word.length).each do |i|
       word_frag = word[0...i] + word[i + 1..-1]
       test_frag = test_word[0...i] + test_word[i + 1..-1]
-      return true if word_frag == test_frag
+      return true if word_frag == test_frag && word != test_word
     end
 
     false
