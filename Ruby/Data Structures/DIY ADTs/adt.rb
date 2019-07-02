24cb5d7ffaf64ea0dd9ce1fed9fda4f42b14ddd5
Matt Ramos <mattramos@gmail.com>
2019-07-01 23:05:12 -0400

    Create a Queue class

--- Ruby/Data Structures/DIY ADTs/adt.rb
+++ Ruby/Data Structures/DIY ADTs/adt.rb
@@ -18,5 +18,22 @@ class Stack
 end
 
 # Create a Queue Class
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(element)
+    @queue << element
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue[0]
+  end
+end
 
 # Create a Map Class
