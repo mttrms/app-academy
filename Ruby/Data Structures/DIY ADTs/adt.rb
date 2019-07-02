efceb18ec98e4cd7a628ea52dd803c38af552d47
Matt Ramos <mattramos@gmail.com>
2019-07-01 23:02:17 -0400

    Create a Stack class

--- Ruby/Data Structures/DIY ADTs/adt.rb
+++ Ruby/Data Structures/DIY ADTs/adt.rb
@@ -1,4 +1,21 @@
 # Create a Stack Class
+class Stack
+  def initialize
+    @elements = []
+  end
+
+  def push(element)
+    @elements << element
+  end
+
+  def pop
+    @elements.pop
+  end
+
+  def peek
+    @elements.last
+  end
+end
 
 # Create a Queue Class
 
