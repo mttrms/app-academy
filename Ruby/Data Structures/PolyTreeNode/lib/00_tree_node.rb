3144735375f1a0516cf2e0e903383c6e1660bb5d
Matt Ramos <mattramos@gmail.com>
2019-07-02 21:31:23 -0400

    Basic PolyTree implementation

--- Ruby/Data Structures/PolyTreeNode/lib/00_tree_node.rb
+++ Ruby/Data Structures/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,34 @@
 class PolyTreeNode
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
 
+  def parent
+    @parent
+  end
+
+  def parent=(node)
+    if @parent == node
+      return
+    elsif node.nil?
+      @parent = nil
+    elsif @parent && node
+      @parent.children.delete(self)
+      @parent = node
+      @parent.children << self
+    else
+      @parent = node
+      @parent.children << self
+    end
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
 end
