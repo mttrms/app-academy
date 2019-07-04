0dd0f518ce11c974e06e787ed9dcfa5a675bd462
matt <mattramos@gmail.com>
2019-07-04 18:46:09 -0400

    Create initialize method to hold TTT game state

--- Ruby/Data Structures/Tic Tac Toe AI/lib/tic_tac_toe_node.rb
+++ Ruby/Data Structures/Tic Tac Toe AI/lib/tic_tac_toe_node.rb
@@ -2,6 +2,10 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = nil || prev_move_pos
+    children
   end
 
   def losing_node?(evaluator)
@@ -13,5 +17,9 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    p @board
   end
 end
+
+game = Board.new
+game_state = TicTacToeNode.new(game, :x)
