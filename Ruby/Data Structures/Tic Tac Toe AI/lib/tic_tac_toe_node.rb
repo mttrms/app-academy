4bdc2df2f6d83451e25d16370f540d452b0a07dd
matt <mattramos@gmail.com>
2019-07-05 09:29:41 -0400

    Generate all possible next move game states

--- Ruby/Data Structures/Tic Tac Toe AI/lib/tic_tac_toe_node.rb
+++ Ruby/Data Structures/Tic Tac Toe AI/lib/tic_tac_toe_node.rb
@@ -5,7 +5,6 @@ class TicTacToeNode
     @board = board
     @next_mover_mark = next_mover_mark
     @prev_move_pos = nil || prev_move_pos
-    children
   end
 
   def losing_node?(evaluator)
@@ -17,9 +16,32 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-    p @board
+    next_move_states = []
+
+    (0..2).each do |row|
+      (0..2).each do |col|
+        pos = [row, col]
+        if @board.empty?(pos)
+          new_board = @board.dup
+          new_board[pos] = @next_mover_mark
+          next_move = TicTacToeNode.new(new_board, swapped_mark, pos)
+          next_move_states << next_move
+        end
+      end
+    end
+
+    next_move_states
+  end
+
+  def swapped_mark
+    if @next_mover_mark == :x
+      :o
+    else
+      :x
+    end
   end
 end
 
 game = Board.new
 game_state = TicTacToeNode.new(game, :x)
+p game_state.children
