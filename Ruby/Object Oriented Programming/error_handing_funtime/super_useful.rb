36deae283e69dfd838efa74510d0304eb542a7d7
mttrms <matt.ramos@policygenius.com>
2019-07-15 08:30:52 -0400

    Error handling in BestFriend class

--- Ruby/Object Oriented Programming/error_handing_funtime/super_useful.rb
+++ Ruby/Object Oriented Programming/error_handing_funtime/super_useful.rb
@@ -40,6 +40,10 @@ end
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new("'name' cannot be blank") if name.empty?
+    raise ArgumentError.new("'yrs_known' must be greater than or equal to 5 (best friendships take time)") if yrs_known.to_i < 5
+    raise ArgumentError.new("'fav_pasttime' cannot be blank") if fav_pastime.empty?
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
--- Ruby/Object Oriented Programming/error_handing_funtime/user_script.rb
+++ Ruby/Object Oriented Programming/error_handing_funtime/user_script.rb
@@ -4,7 +4,7 @@ puts "'five' == #{convert_to_int('five')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('Sam', 5, 'Flying kites')
 
 sam.talk_about_friendship
 sam.do_friendstuff
