09048b84590b82fd837a612881e45ceb934be095
mttrms <matt.ramos@policygenius.com>
2019-07-15 19:46:47 -0400

    Add [], []= and #move_piece

--- Ruby/Object Oriented Programming/chess/board.rb
+++ Ruby/Object Oriented Programming/chess/board.rb
@@ -10,5 +10,22 @@ class Board
     end
   end
 
+  def [](pos)
+    row, col = pos
+    rows[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    rows[row][col] = val
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise ArgumentError.new("There is no piece at #{start_pos}") if self[start_pos] == nil
+    raise ArgumentError.new("This piece cannot move to #{end_pos}") if self[end_pos] != nil
+
+    self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
+  end
+
   STARTING_ROWS = [0, 1, 6, 7]
 end
