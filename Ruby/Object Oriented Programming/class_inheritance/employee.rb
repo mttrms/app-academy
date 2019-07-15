8dab5e40252a65b45607d751acda12c072ac313c
mttrms <matt.ramos@policygenius.com>
2019-07-15 08:44:17 -0400

    Write simple employee class for inheritance exercise

---
+++ Ruby/Object Oriented Programming/class_inheritance/employee.rb
@@ -0,0 +1,12 @@
+class Employee
+    def initialize(name, title, salary, boss = nil)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+
+    end
+end
