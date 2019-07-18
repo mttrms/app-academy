e89bd083f4a8bdbbefed5560e47aa54207d82b6b
mttrms <matt.ramos@policygenius.com>
2019-07-17 20:42:48 -0400

    Make pawns aware of their forward direction, add forward moves

--- Ruby/Object Oriented Programming/chess/pieces/pawn.rb
+++ Ruby/Object Oriented Programming/chess/pieces/pawn.rb
@@ -1,19 +1,24 @@
 class Pawn < Piece
   def move_dirs
-    []
+    forward_steps
   end
 
   private
-
   def at_start_row?
     pos[0] == 1 || pos[0] == 6
   end
 
   def forward_dir
-    # returns 1 or -1
+    @color == :white ? -1 : 1
   end
 
   def forward_steps
+    moves = []
+    col, row = pos
+    moves << [col + forward_dir, row]
+    moves << [col + (forward_dir * 2), row] if at_start_row?
+
+    moves
   end
 
   def side_attacks
