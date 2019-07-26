198921115ba814710d9d4f4baff2c1b639afcb43
Matt Ramos <mattramos@gmail.com>
2019-07-26 09:07:06 -0400

    Work on RSpec homework

--- Ruby/RSpec & TDD/dessert/spec/dessert_spec.rb
+++ Ruby/RSpec & TDD/dessert/spec/dessert_spec.rb
@@ -6,16 +6,25 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
+  subject(:dessert) { Dessert.new("brownie", 50, :chef) }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(dessert.type).to eq("brownie")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(50)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty 
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      
+    end
   end
 
   describe "#add_ingredient" do
