a0afbae727230542b097de1ed5b4b58faaae9085
Matt Ramos <mattramos@gmail.com>
2019-07-27 13:08:07 -0400

    Two sum TDD

--- Ruby/RSpec & TDD/tdd/lib/tdd.rb
+++ Ruby/RSpec & TDD/tdd/lib/tdd.rb
@@ -3,3 +3,21 @@ def my_uniq(arr)
   arr.each { |el| uniq << el unless uniq.include?(el) }
   return uniq
 end
+
+class Array
+  def two_sum
+    pairs = []
+
+    self.each_with_index do |num, idx|
+      self.each_with_index do |num2, idx2|
+        next if idx == idx2
+
+        if num + num2 == 0 && pairs.none? { |el| el.include?(idx) }
+          pairs << [idx, idx2]
+        end
+      end
+    end
+
+    pairs
+  end
+end
--- Ruby/RSpec & TDD/tdd/spec/tdd_spec.rb
+++ Ruby/RSpec & TDD/tdd/spec/tdd_spec.rb
@@ -15,3 +15,27 @@ describe "#my_uniq" do
     expect(my_uniq(complex_array)).to eq([5,0,1,2,4,7])
   end
 end 
+
+describe "Array#two_sum" do
+  my_arr = [-1, 0, 2, -2, 1]
+
+  it "exists as an Array method" do
+    expect(my_arr).to respond_to(:two_sum)
+  end
+
+  it "returns an array" do
+    expect(my_arr.two_sum).to be_an_instance_of(Array)  
+  end
+
+  it "returns a new array" do
+    expect(my_arr.two_sum).not_to be(my_arr)
+  end
+
+  it "sorts each pair smallest to largest" do
+    expect([-1, 3, 3, 3, 1].two_sum).to eq([[0, 4]])
+  end
+
+  it "sorts the array of pairs smallest to largest" do
+    expect(my_arr.two_sum).to eq([[0, 4], [2, 3]])
+  end
+end
