b9f6ad0b683b868111dcb384d7c79492d1bdc47b
Matt Ramos <mattramos@gmail.com>
2019-08-05 08:00:32 -0400

    Anagrams phase 2

--- Ruby/Big-O/anagrams.rb
+++ Ruby/Big-O/anagrams.rb
@@ -6,3 +6,17 @@ def first_anagram?(str1, str2)
 end
 
 p first_anagram?("present", "serpent")
+
+# O(n) time
+def second_anagram?(str1, str2)
+  return false if str1.length != str2.length
+
+  str2 = str2.split("")
+  str1.each_char do |char|
+    str2.delete(char) if str2.include?(char)
+  end
+
+  str2.empty?
+end
+
+p second_anagram?("present", "serpent")
