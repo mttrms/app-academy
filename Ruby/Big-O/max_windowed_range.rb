5ead08eb9ce7b981b8a79955da52c133d87df8f5
Matt Ramos <mattramos@gmail.com>
2019-08-05 23:07:58 -0400

    MyStack

--- Ruby/Big-O/max_windowed_range.rb
+++ Ruby/Big-O/max_windowed_range.rb
@@ -46,3 +46,29 @@ class MyQueue
     @store.shift
   end
 end
+
+class MyStack
+  def initialize
+    @store = []
+  end
+
+  def peek
+    @store.last
+  end
+
+  def size
+    @store.size
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def push(element)
+    @store << element
+  end
+end
