faffda7e4d549177f642677e2886c950fb1a61d5
Matt Ramos <mattramos@gmail.com>
2019-08-05 23:02:51 -0400

    MyQueue

--- Ruby/Big-O/max_windowed_range.rb
+++ Ruby/Big-O/max_windowed_range.rb
@@ -20,3 +20,29 @@ p max_windowed_range([1, 0, 2, 5, 4, 8], 2)
 p max_windowed_range([1, 0, 2, 5, 4, 8], 3)
 p max_windowed_range([1, 0, 2, 5, 4, 8], 4)
 p max_windowed_range([1, 3, 2, 5, 4, 8], 5)
+
+class MyQueue
+  def initialize
+    @store = []
+  end
+
+  def peek
+    @store.first
+  end
+
+  def size
+    @store.size
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def enqueue(element)
+    @store << element
+  end
+
+  def dequeue
+    @store.shift
+  end
+end
