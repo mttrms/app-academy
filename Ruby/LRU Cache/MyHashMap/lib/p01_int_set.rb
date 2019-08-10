73d4d7eaa639a10f0741093eebd34b24997be7af
Matt Ramos <mattramos@gmail.com>
2019-08-10 11:24:17 -0400

    Complete MaxIntSet

--- Ruby/LRU Cache/MyHashMap/lib/p01_int_set.rb
+++ Ruby/LRU Cache/MyHashMap/lib/p01_int_set.rb
@@ -1,22 +1,32 @@
 class MaxIntSet
+  attr_reader :store
+
   def initialize(max)
+    @store = Array.new(max + 1)
   end
 
   def insert(num)
+    raise ArgumentError, "Out of bounds" unless is_valid?(num)
+
+    @store[num] = true
   end
 
   def remove(num)
+    raise ArgumentError, "Out of bounds" unless is_valid?(num)
+
+    @store[num] = false
   end
 
   def include?(num)
+    raise ArgumentError, "Out of bounds" unless is_valid?(num)
+
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
-  end
-
-  def validate!(num)
+    num >= 0 && num < @store.length
   end
 end
 
