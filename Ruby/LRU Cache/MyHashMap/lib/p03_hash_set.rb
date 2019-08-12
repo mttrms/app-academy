5bc6a83c867e4b6d373912f7bea915cf8963936a
Matt Ramos <mattramos@gmail.com>
2019-08-11 20:35:46 -0400

    Complete HashSet

--- Ruby/LRU Cache/MyHashMap/lib/p03_hash_set.rb
+++ Ruby/LRU Cache/MyHashMap/lib/p03_hash_set.rb
@@ -7,18 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      self[key.hash] << key
+      @count += 1
+    end
+
+    resize! if @count == num_buckets
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +37,10 @@ class HashSet
   end
 
   def resize!
+    prev_buckets = @store.dup
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+
+    prev_buckets.flatten.each { |ele| insert(ele) }
   end
 end
