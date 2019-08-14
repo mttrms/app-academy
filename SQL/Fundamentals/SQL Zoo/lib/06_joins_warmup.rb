11469663790981bcf153ab19eb09105056c373c1
Matt Ramos <mattramos@gmail.com>
2019-08-13 23:22:49 -0400

    Complete joins warmup

--- SQL/Fundamentals/SQL Zoo/lib/06_joins_warmup.rb
+++ SQL/Fundamentals/SQL Zoo/lib/06_joins_warmup.rb
@@ -36,12 +36,24 @@ end
 def films_from_sixty_two
   # List the films where the yr is 1962 [Show id, title]
   execute(<<-SQL)
+  SELECT
+    id, title
+  FROM
+    movies
+  WHERE
+    yr = 1962
   SQL
 end
 
 def year_of_kane
   # Give year of 'Citizen Kane'.
   execute(<<-SQL)
+  SELECT
+    yr
+  FROM
+    movies
+  WHERE
+    title = 'Citizen Kane'
   SQL
 end
 
@@ -50,24 +62,50 @@ def trek_films
   # these movies include the words Star Trek in the title). Order results by
   # year.
   execute(<<-SQL)
+  SELECT
+    id, title, yr
+  FROM
+    movies
+  WHERE
+    title like '%Star Trek%'
+  ORDER BY
+    yr ASC
   SQL
 end
 
 def films_by_id
   # What are the titles of the films with id 1119, 1595, 1768?
   execute(<<-SQL)
+  SELECT
+    title
+  FROM
+    movies
+  WHERE
+    id in (1119, 1595, 1768)
   SQL
 end
 
 def glenn_close_id
   # What id number does the actress 'Glenn Close' have?
   execute(<<-SQL)
+  SELECT
+    id
+  FROM
+    actors
+  WHERE
+    name = 'Glenn Close'
   SQL
 end
 
 def casablanca_id
   # What is the id of the film 'Casablanca'?
   execute(<<-SQL)
+  SELECT
+    id
+  FROM
+    movies
+  WHERE
+    title = 'Casablanca'
   SQL
 end
 
@@ -75,11 +113,29 @@ def casablanca_cast
   # Obtain the cast list for 'Casablanca'. Use the id value that you obtained
   # in the previous question directly in your query (for example, id = 1).
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    actors
+  JOIN
+    castings ON actors.id = castings.actor_id
+  WHERE
+    castings.movie_id = 27
   SQL
 end
 
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    actors
+  JOIN
+    castings ON actors.id = castings.actor_id
+  JOIN
+    movies ON castings.movie_id = movies.id
+  WHERE
+    title = 'Alien'
   SQL
 end
