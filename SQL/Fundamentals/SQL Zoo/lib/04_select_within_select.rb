a68c8188b8e19d91a653a2a8fed80ca9974f69ab
Matt Ramos <mattramos@gmail.com>
2019-08-13 20:45:59 -0400

    Complete select within select

--- SQL/Fundamentals/SQL Zoo/lib/04_select_within_select.rb
+++ SQL/Fundamentals/SQL Zoo/lib/04_select_within_select.rb
@@ -35,6 +35,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    population > (
+      SELECT
+        population
+      FROM
+        countries
+      WHERE
+        name = 'Russia'
+        )
   SQL
 end
 
@@ -42,6 +55,21 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    (gdp / population) > (
+      SELECT
+        (gdp / population)
+      FROM
+        countries
+      WHERE
+        name = 'United Kingdom'
+      )
+  AND
+    continent = 'Europe'
   SQL
 end
 
@@ -49,6 +77,19 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    continent in (
+      SELECT
+        continent
+      FROM
+        countries
+      WHERE
+        name in ('Belize', 'Belgium')
+      )
   SQL
 end
 
@@ -56,6 +97,28 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT
+    name, population
+  FROM
+    countries
+  WHERE
+    population >
+      ( SELECT
+          population
+        FROM
+          countries
+        WHERE
+         name = 'Canada'
+      )
+  AND
+    population <
+      ( SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Poland'
+      )
   SQL
 end
 
@@ -65,5 +128,19 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT
+    name, continent, population
+  FROM
+    countries
+  WHERE
+    continent in (
+      SELECT
+        continent
+      FROM
+        countries
+      GROUP BY
+        continent
+      HAVING MAX(population) < 25000000
+      )
   SQL
 end
