b42996705853d7e4f9bcfc7379676826d17bed38
Matt Ramos <mattramos@gmail.com>
2019-08-21 09:23:15 -0400

    Complete most associations

--- SQL/Active Record I/associations exercise/app/models/course.rb
+++ SQL/Active Record I/associations exercise/app/models/course.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ApplicationRecord
+  has_many :enrollments
+  has_many :enrolled_students, through: :enrollments,
+    source: :user
+
+  has_one :prerequisite,
+    primary_key: :prereq_id,
+    foreign_key: :id,
+    class_name: :Course
 end
--- SQL/Active Record I/associations exercise/app/models/enrollment.rb
+++ SQL/Active Record I/associations exercise/app/models/enrollment.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Enrollment < ApplicationRecord
+  belongs_to :course
+  belongs_to :user,
+    foreign_key: :student_id
 end
