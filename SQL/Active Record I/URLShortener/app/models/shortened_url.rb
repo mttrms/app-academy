602948c0d084d1a8e21abee47da00e79052003e5
Matt Ramos <mattramos@gmail.com>
2019-08-21 21:01:12 -0400

    Create ShortenedUrl, validations and associations with users

--- SQL/Active Record I/URLShortener/app/models/shortened_url.rb
+++ SQL/Active Record I/URLShortener/app/models/shortened_url.rb
@@ -1,3 +1,27 @@
 class ShortenedUrl < ApplicationRecord
+  belongs_to :submitter,
+    class_name: 'User',
+    foreign_key: :user_id
+
   validates :long_url, presence: true, uniqueness: true
+
+  def self.random_code
+    code = nil
+    while code == nil || ShortenedUrl.exists?(:short_url => code)
+      code = SecureRandom::urlsafe_base64
+    end
+
+    code
+  end
+
+  def self.create_shortened_url!(options)
+    user = options[:user]
+    long_url = options[:long_url]
+
+    ShortenedUrl.create!(
+      long_url: long_url,
+      short_url: ShortenedUrl.random_code,
+      user_id: user.id
+    )
+  end
 end
--- SQL/Active Record I/URLShortener/app/models/user.rb
+++ SQL/Active Record I/URLShortener/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :shortened_urls
+
   validates :email, presence: true, uniqueness: true
 end
