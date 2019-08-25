c789a9029df4d2346630e49363280efaff6c70e5
Matt Ramos <mattramos@gmail.com>
2019-08-25 17:57:45 -0400

    Scopes

--- SQL/Active Record II/joins_demo/app/models/post.rb
+++ SQL/Active Record II/joins_demo/app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
 
   belongs_to :author, :class_name => "User"
   has_many :comments, :foreign_key => :post_id
+
+  def self.by_popularity
+    self
+    .select('posts.*, COUNT(*) AS comment_count')
+    .joins(:comments)
+    .group('posts.id')
+    .order('comment_count DESC')
+  end
 end
