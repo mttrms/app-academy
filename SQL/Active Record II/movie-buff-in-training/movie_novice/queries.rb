5585dc9f8e356d9f10658acd2866eea393ff653c
Matt Ramos <mattramos@gmail.com>
2019-08-26 20:17:49 -0400

    Complete Movie Buff in training

--- SQL/Active Record II/movie-buff-in-training/movie_novice/queries.rb
+++ SQL/Active Record II/movie-buff-in-training/movie_novice/queries.rb
@@ -53,14 +53,14 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor.select(:id, :name).order('name ASC').limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor.select('actors.id, actors.name').joins(:movies).where('movies.title = \'Pulp Fiction\'')
 end
 
 def uma_movies
@@ -68,5 +68,5 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie.select('movies.id, movies.title, movies.yr').joins(:actors).where('actors.name = \'Uma Thurman\'').order('yr ASC')
 end
