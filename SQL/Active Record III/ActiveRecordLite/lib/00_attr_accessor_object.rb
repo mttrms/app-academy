79c129886c4cc3e4638bfa09f0a52228737bf230
Matt Ramos <mattramos@gmail.com>
2019-09-08 22:28:29 -0400

    Create getter methods

--- SQL/Active Record III/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ SQL/Active Record III/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,9 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+    end
   end
 end
