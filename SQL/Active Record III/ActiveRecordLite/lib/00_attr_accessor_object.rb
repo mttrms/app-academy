3d2c800558d83493517df7404d10b09c2f709814
Matt Ramos <mattramos@gmail.com>
2019-09-08 22:40:35 -0400

    Create setter methods

--- SQL/Active Record III/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ SQL/Active Record III/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -4,6 +4,10 @@ class AttrAccessorObject
       define_method(name) do
         instance_variable_get("@#{name}")
       end
+
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
     end
   end
 end
