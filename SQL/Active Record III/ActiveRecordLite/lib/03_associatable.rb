7dedba5052065df93fdc6082cf9765bb6733ab30
Matt Ramos <mattramos@gmail.com>
2019-09-10 09:00:40 -0400

    Set HasManyOptions defaults

--- SQL/Active Record III/ActiveRecordLite/lib/03_associatable.rb
+++ SQL/Active Record III/ActiveRecordLite/lib/03_associatable.rb
@@ -20,15 +20,17 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    @class_name = ActiveSupport::Inflector.camelize((options[:class_name] || name))
-    @foreign_key = "#{ActiveSupport::Inflector.underscore(class_name)}_id".to_sym
+    @class_name = (options[:class_name] || name).camelize
+    @foreign_key = "#{class_name.underscore}_id".to_sym
     @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = (options[:class_name] || name).singularize.camelize
+    @foreign_key = options[:foreign_key] || "#{self_class_name}_id".underscore.to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
