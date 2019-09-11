0fa9acb2e5904b0673f43d721f23acc25b1f0630
Matt Ramos <mattramos@gmail.com>
2019-09-11 00:43:47 -0400

    Refactor #has_one_through

--- SQL/Active Record III/ActiveRecordLite/lib/04_associatable2.rb
+++ SQL/Active Record III/ActiveRecordLite/lib/04_associatable2.rb
@@ -10,20 +10,24 @@ module Associatable
       source_options = through_options.model_class.assoc_options[source_name]
 
       # Set required tables & keys for query
-      source_table_name = source_options.table_name
-      source_class = source_options.model_class
+      source_table = source_options.table_name
+      source_pk = source_options.primary_key
       source_fk = source_options.foreign_key
-      through_table_name = through_options.table_name
+
+      through_table = through_options.table_name
       through_pk = through_options.primary_key
 
-      results = DBConnection.execute(<<-SQL, self.attributes[through_pk])
-        SELECT #{source_table_name}.*
-        FROM #{through_table_name}
-        JOIN #{source_table_name} ON #{through_table_name}.#{source_fk} = #{source_table_name}.#{through_pk}
-        WHERE #{through_table_name}.#{through_pk} = ?
+      key_val = self.attributes[through_pk]
+
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT #{source_table}.*
+        FROM #{through_table}
+        JOIN #{source_table}
+        ON #{through_table}.#{source_fk} = #{source_table}.#{source_pk}
+        WHERE #{through_table}.#{through_pk} = ?
       SQL
 
-      source_class.parse_all(results).first
+      source_options.model_class.parse_all(results).first
     end
   end
 end
