e74ce5f17210068e4bed546fac015ad8b3972baa
Matt Ramos <mattramos@gmail.com>
2019-09-18 08:20:57 -0400

    Create new cats via forms

--- Rails/Routes and Controllers/cat_api/Gemfile
+++ Rails/Routes and Controllers/cat_api/Gemfile
@@ -31,6 +31,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 group :development do
--- Rails/Routes and Controllers/cat_api/app/controllers/cats_controller.rb
+++ Rails/Routes and Controllers/cat_api/app/controllers/cats_controller.rb
@@ -9,20 +9,44 @@ class CatsController < ApplicationController
     render :show
   end
 
+  # 1. GET /cat/news to fetch a form
+  # 2. Fill out form, click submit
+  # 3. POST /cats the data in the form
+  # 4. Invoke create action
+  # 5. Redirect to /cats/#{id}
+
+  def new
+    # /cats/new
+    # show a form to create a new object
+    render :new
+  end
+
   def create
     # POST /cats
     # { "cat": { "name": "Sally" } }
-    cat = Cat.new(name: params[:cat][:name])
-    if cat.save
-      render json: cat
+    @cat = Cat.new(name: params[:cat][:name])
+
+    if @cat.save
+      redirect_to cat_url(@cat)
     else
       render json: cat.errors.full_messages, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    # /cats/:id/edit
+    # show a form to edit an existing object
   end
 
   def update
   end
 
   def destroy
+    # DELETE /cats/:id
+    # _method="DELETE" so rails understands to do a destroy
+    cat = Cat.find(params[:id])
+    cat.destroy
+
+    redirect_to cats_url
   end
 end
