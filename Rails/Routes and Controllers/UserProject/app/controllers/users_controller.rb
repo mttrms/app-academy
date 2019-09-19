81cea21a9c6b58a68fd6bb7be95b2d4aa9a10775
Matt Ramos <mattramos@gmail.com>
2019-09-19 09:19:49 -0400

    Create users

--- Rails/Routes and Controllers/UserProject/app/controllers/users_controller.rb
+++ Rails/Routes and Controllers/UserProject/app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    render json: params
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
