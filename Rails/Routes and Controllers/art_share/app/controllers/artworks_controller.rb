ff2b6a3391d6d33fa5139515800fddba52265b2c
Matt Ramos <mattramos@gmail.com>
2019-09-21 18:40:25 -0400

    Add artwork endpoints

---
+++ Rails/Routes and Controllers/art_share/app/controllers/artworks_controller.rb
@@ -0,0 +1,42 @@
+class ArtworksController < ApplicationController
+  def index
+    @artworks = Artwork.all
+    render json: @artworks 
+  end
+
+  def show
+    @artwork = Artwork.find(params[:id])
+    render json: @artwork
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @artwork = Artwork.find(params[:id])
+    
+    if @artwork.update!(artwork_params)
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @artwork = Artwork.find(params[:id])
+    @artwork.destroy
+
+    render json: @artwork
+  end
+
+  private
+  def artwork_params
+    params[:artwork].permit(:title, :image_url, :artist_id)
+  end
+end
--- Rails/Routes and Controllers/art_share/config/routes.rb
+++ Rails/Routes and Controllers/art_share/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :artworks, only: [:index, :show, :create, :update, :destroy]
 end
