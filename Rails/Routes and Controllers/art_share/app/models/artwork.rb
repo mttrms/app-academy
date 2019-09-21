ba5b46ec52d8831c7828ab4143514c10c7de16c0
Matt Ramos <mattramos@gmail.com>
2019-09-21 14:39:25 -0400

    Add validations to Artworks

--- Rails/Routes and Controllers/art_share/app/models/artwork.rb
+++ Rails/Routes and Controllers/art_share/app/models/artwork.rb
@@ -1,3 +1,8 @@
 class Artwork < ApplicationRecord
-  validates :title, :image_url, :artist_id, presence: true
+  belongs_to :artist,
+    class_name: 'User',
+    foreign_key: :artist_id
+
+  validates :title, :image_url, :artist_id,  presence: true
+  validates :title, uniqueness: { scope: :artist }
 end
