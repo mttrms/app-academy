ee0f36511729ce0e173d97367049f4c3cb62a78c
Matt Ramos <mattramos@gmail.com>
2019-09-21 18:41:09 -0400

    Update user variables

--- Rails/Routes and Controllers/art_share/app/controllers/users_controller.rb
+++ Rails/Routes and Controllers/art_share/app/controllers/users_controller.rb
@@ -5,36 +5,35 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
     else
-      render json: user.errors.full_messages, status: :unprocessable_entity
+      render json: @user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
+    @user = User.find(params[:id])
+    @user.update(user_params)
 
-    render json: user
+    render json: @user
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
+    @user = User.find(params[:id])
+    @user.destroy
 
-    render json: user
+    render json: @user
   end
 
   private
   def user_params
     params[:user].permit(:username)
   end
-
 end
