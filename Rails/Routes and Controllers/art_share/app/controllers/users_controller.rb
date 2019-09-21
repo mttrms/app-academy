05d05d5a6ab0c460140b473bdf60a8d494caaf9c
Matt Ramos <mattramos@gmail.com>
2019-09-21 17:11:17 -0400

    Minor User refactor

--- Rails/Routes and Controllers/art_share/app/controllers/users_controller.rb
+++ Rails/Routes and Controllers/art_share/app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params[:user].permit(:name, :email)
+    params[:user].permit(:username)
   end
 
 end
--- Rails/Routes and Controllers/art_share/config/routes.rb
+++ Rails/Routes and Controllers/art_share/config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # resources :users
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new', as: 'new_user'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  patch 'users/:id', to: 'users#update'
-  put 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
+  resources :users, only: [:index, :show, :create, :update, :destroy]
 end
