9f3a2a930b09660e47ed8794aa722242708c81a0
matt <mattramos@gmail.com>
2019-09-22 23:52:12 -0400

    Create CatRentalRequests migration

--- Rails/Rails Views/ninety_nine_cats/db/schema.rb
+++ Rails/Rails Views/ninety_nine_cats/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_232221) do
+ActiveRecord::Schema.define(version: 2019_09_23_034815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_rental_requests", force: :cascade do |t|
+    t.bigint "cat_id", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.string "status", default: "PENDING", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cat_id"], name: "index_cat_rental_requests_on_cat_id"
+  end
 
   create_table "cats", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2019_09_22_232221) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cat_rental_requests", "cats"
 end
