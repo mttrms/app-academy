6b3e377cbea26fd380cabfb7d95595b612fff943
matt <mattramos@gmail.com>
2019-09-23 00:42:50 -0400

    Add overlapping request validations

--- Rails/Rails Views/ninety_nine_cats/app/models/cat_rental_request.rb
+++ Rails/Rails Views/ninety_nine_cats/app/models/cat_rental_request.rb
@@ -4,4 +4,21 @@ class CatRentalRequest < ApplicationRecord
   belongs_to :cat
   validates :cat_id, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: { in: STATUS }
+  validate :does_not_overlap_approved_request
+
+  def overlapping_requests
+    CatRentalRequest
+      .where.not('(start_date > :start AND end_date > :start) OR (start_date < :end AND end_date < :end)', start: start_date, end: end_date)
+      .where(cat_id: cat_id)
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where(status: 'APPROVED')
+  end
+
+  def does_not_overlap_approved_request
+    if overlapping_approved_requests.any?
+      errors[:base] << "overlaps an approved rental request"
+    end
+  end
 end
