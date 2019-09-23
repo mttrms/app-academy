1b151ab3fd58861518d27ce180edacdb67b5f045
matt <mattramos@gmail.com>
2019-09-22 21:47:34 -0400

    Create cat#new form

--- Rails/Rails Views/ninety_nine_cats/app/controllers/cats_controller.rb
+++ Rails/Rails Views/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -8,4 +8,17 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    render json: cat_params
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :gender, :color, :birth_date, :description)
+  end
 end
--- Rails/Rails Views/ninety_nine_cats/config/routes.rb
+++ Rails/Rails Views/ninety_nine_cats/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :cats, only: [:index, :show]
+  resources :cats, only: [:index, :show, :new, :create]
 end
