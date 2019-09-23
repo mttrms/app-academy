5e01f3ea4ae95103a788b2b66309cd8ffc097e81
matt <mattramos@gmail.com>
2019-09-22 20:22:13 -0400

    Complete Cat model

--- Rails/Rails Views/ninety_nine_cats/app/models/cat.rb
+++ Rails/Rails Views/ninety_nine_cats/app/models/cat.rb
@@ -1,8 +1,13 @@
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  COLORS = %w(white black red gray cream brown cinnamon)
+
   validates :name, :birth_date, :sex, :color, :description, presence: true
-  validates :color, inclusion: { in: %w(white black red gray cream brown cinnamon)}
+  validates :color, inclusion: { in: COLORS}
   validates :sex, inclusion: { in: %w(m f)}
 
   def age
+    time_ago_in_words(birth_date)
   end
 end
