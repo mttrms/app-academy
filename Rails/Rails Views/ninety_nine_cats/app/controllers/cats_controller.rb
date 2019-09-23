2ef3f4d0bf7f8a7043cbb43771684a38868d90a0
matt <mattramos@gmail.com>
2019-09-22 23:22:05 -0400

    Cats can be edited

--- Rails/Rails Views/ninety_nine_cats/app/controllers/cats_controller.rb
+++ Rails/Rails Views/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -22,6 +22,20 @@ class CatsController < ApplicationController
     end
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render json: @cat
+    end
+  end
+
   private
   def cat_params
     params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
--- Rails/Rails Views/ninety_nine_cats/config/routes.rb
+++ Rails/Rails Views/ninety_nine_cats/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :cats, only: [:index, :show, :new, :create]
+  resources :cats, only: [:index, :show, :new, :create, :edit, :update]
 end
