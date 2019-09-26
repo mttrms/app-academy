8795a26b5d01e38aef6268dfc30e5c069f97f286
Matt Ramos <mattramos@gmail.com>
2019-09-26 07:56:00 -0400

    Create user model and migrations

---
+++ Rails/Rails Auth/auth_project/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+end
---
+++ Rails/Rails Auth/auth_project/db/migrate/20190926114004_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest
+      t.string :session_token
+
+      t.index :username
+      t.timestamps
+    end
+  end
+end
