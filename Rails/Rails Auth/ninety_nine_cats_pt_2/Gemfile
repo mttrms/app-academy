0646aa55743c12c9ab72ae9e7f624183b9de8033
Matt Ramos <mattramos@gmail.com>
2019-09-26 08:57:51 -0400

    Add passwords to Users

--- Rails/Rails Auth/ninety_nine_cats_pt_2/Gemfile
+++ Rails/Rails Auth/ninety_nine_cats_pt_2/Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'bcrypt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- Rails/Rails Auth/ninety_nine_cats_pt_2/app/models/user.rb
+++ Rails/Rails Auth/ninety_nine_cats_pt_2/app/models/user.rb
@@ -7,10 +7,6 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
-  def self.generate_session_token
-    SecureRandom::urlsafe_base64
-  end
-
   def ensure_session_token
     self.session_token ||= User.generate_session_token
   end
@@ -18,4 +14,25 @@ class User < ApplicationRecord
   def reset_session_token!
     self.session_token = User.generate_session_token
   end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    digest = BCrypt::Password.new(self.password_digest)
+    digest.is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+  
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
 end
