313e1e65584b3a91c0f6673372dc0fe91f95ba09
Matt Ramos <mattramos@gmail.com>
2019-09-26 08:49:27 -0400

    Add session token methods

--- Rails/Rails Auth/ninety_nine_cats_pt_2/app/models/user.rb
+++ Rails/Rails Auth/ninety_nine_cats_pt_2/app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   validates :user_name, :session_token, presence: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
 end
