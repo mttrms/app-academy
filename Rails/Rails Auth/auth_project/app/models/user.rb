eb9497a477f4d3b4faed0e6b416941dcbfd4c01a
Matt Ramos <mattramos@gmail.com>
2019-09-26 08:22:19 -0400

    Complete auth project

--- Rails/Rails Auth/auth_project/app/models/user.rb
+++ Rails/Rails Auth/auth_project/app/models/user.rb
@@ -4,4 +4,29 @@ class User < ApplicationRecord
   validates :username, :session_token, presence: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    BCrypt::Password.new(user.password_digest).is_password?(password) ? user : 'not valid'
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
