bb86c72912519b1ef44870dc5d9ff1c7c18bac3c
matt <mattramos@gmail.com>
2019-09-27 16:53:20 -0400

    Destroy sessions, show username and signin/signout links

--- Rails/Rails Auth/ninety_nine_cats_pt_2/app/controllers/application_controller.rb
+++ Rails/Rails Auth/ninety_nine_cats_pt_2/app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    raise "No user logged in" if session[:session_token].nil?
-
     User.find_by(session_token: session[:session_token])
   end
 end
--- Rails/Rails Auth/ninety_nine_cats_pt_2/app/controllers/sessions_controller.rb
+++ Rails/Rails Auth/ninety_nine_cats_pt_2/app/controllers/sessions_controller.rb
@@ -11,6 +11,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to :cats
   end
 
   private
