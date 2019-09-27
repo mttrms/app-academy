f61913554b28d58136f4174ee68e980069b86605
matt <mattramos@gmail.com>
2019-09-27 16:59:39 -0400

    Users can signup

--- Rails/Rails Auth/ninety_nine_cats_pt_2/app/controllers/users_controller.rb
+++ Rails/Rails Auth/ninety_nine_cats_pt_2/app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:session_token] = @user.session_token
+      redirect_to :cats
+    else
+      render json: @user.errors.full_messages
+    end
+    # render json: user_params
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password)
   end
 end
