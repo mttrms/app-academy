15daa100a798e23900dc9122465bf1d744d70117
Matt Ramos <mattramos@gmail.com>
2019-10-02 08:14:28 -0400

    Set passwords and validate users

--- Rails/Rails Auth/music_app/app/models/user.rb
+++ Rails/Rails Auth/music_app/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   after_initialize :ensure_session_token
 
-  def self.generate_session_token
-    SecureRandom.urlsafe_base64(20)
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
   end
 
   def reset_session_token!
@@ -11,7 +11,22 @@ class User < ApplicationRecord
     self.session_token
   end
 
-  def ensure_session_token
-    self.session_token ||= User.generate_session_token
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_digest = BCrypt::Password.new(self.password_digest)
+    password_digest.is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user.is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(20)
   end
 end
