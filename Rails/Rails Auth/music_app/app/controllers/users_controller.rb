08419a7a4bee99bc2286686999658decbbf774a8
Matt Ramos <mattramos@gmail.com>
2019-10-03 19:37:19 -0400

    Add user profile pages

--- Rails/Rails Auth/music_app/app/controllers/users_controller.rb
+++ Rails/Rails Auth/music_app/app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
   def create
     @user = User.new(user_params)
 
--- Rails/Rails Auth/music_app/config/routes.rb
+++ Rails/Rails Auth/music_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
+  resource :session, only: [:new, :create, :destroy]
 end
