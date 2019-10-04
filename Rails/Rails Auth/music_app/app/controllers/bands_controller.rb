3ec2a47cff7245c63e20de83c85b820c5262608d
Matt Ramos <mattramos@gmail.com>
2019-10-04 08:56:44 -0400

    Finish bands routes and views

--- Rails/Rails Auth/music_app/app/controllers/bands_controller.rb
+++ Rails/Rails Auth/music_app/app/controllers/bands_controller.rb
@@ -1,19 +1,51 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+    render :index
+  end
+
   def new
+    @band = Band.new
+    render :new
   end
 
   def create
+    @band = Band.new(band_params)
+
+    if @band.save
+      redirect_to @band
+    else
+      render :new
+    end
   end
 
   def show
+    @band = Band.find(params[:id])
   end
 
   def edit
+    @band = Band.find(params[:id])
+    render :edit
   end
 
-  def patch
+  def update
+    @band = Band.find(params[:id])
+    if @band.update(band_params)
+      redirect_to @band
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    
+    redirect_to bands_path
+  end
+
+  private
+  def band_params
+    params.require(:band).permit(:name)
   end
 end
