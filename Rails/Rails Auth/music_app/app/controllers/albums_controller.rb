651c92a195d3b4d956385543700632bdeda02af7
matt <mattramos@gmail.com>
2019-10-06 19:43:30 -0400

    Finish album form

--- Rails/Rails Auth/music_app/app/controllers/albums_controller.rb
+++ Rails/Rails Auth/music_app/app/controllers/albums_controller.rb
@@ -7,13 +7,26 @@ class AlbumsController < ApplicationController
     render :new
   end
 
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_path(@album)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
   def edit
   end
 
-  def create
-    render json: params
+  def update
   end
 
-  def update
+  private
+  def album_params
+    params.require(:album).permit(:title, :year, :live, :band_id)
   end
 end
