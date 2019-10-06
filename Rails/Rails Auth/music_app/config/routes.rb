7adccb4680096559ea9d50644b2d7d9cdf6747fe
matt <mattramos@gmail.com>
2019-10-05 22:54:15 -0400

    Update routes for albums

--- Rails/Rails Auth/music_app/config/routes.rb
+++ Rails/Rails Auth/music_app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
-  resources :bands
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+  resources :albums, only: [:create, :edit, :update, :destroy, :show]
   resource :session, only: [:new, :create, :destroy]
 end
