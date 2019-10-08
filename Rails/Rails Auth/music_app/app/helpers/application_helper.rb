b2edd92c78aabe93d309d94bc0fb562346024d21
Matt Ramos <mattramos@gmail.com>
2019-10-08 19:40:13 -0400

    Add ugly_lyrics helper

--- Rails/Rails Auth/music_app/app/helpers/application_helper.rb
+++ Rails/Rails Auth/music_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def ugly_lyrics(lyrics)
+    html = '<div class="lyrics">'
+    lyrics.split("\r\n").each do |line|
+      html += "<p>&#9835; #{h(line)}</p>"
+    end
+    html += '</div>'
+
+    html.html_safe
+  end
 end
