a938bb16d6ae58652a0c26f5cb2997f9ebb8c530
Matt Ramos <mattramos@gmail.com>
2019-10-12 19:07:11 -0400

    Replace with shoulda matcher

--- Rails/Rails Auth/music_app/spec/models/user_spec.rb
+++ Rails/Rails Auth/music_app/spec/models/user_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe User, type: :model do
   let(:valid_user) { User.new(email: 'hunter12@hunter.com', password: 'Hunter12') }
   let(:no_password) { User.new(email: 'hunter12@hunter12.com') }
 
-  it 'validates presence of email' do
-    expect(invalid_user).not_to be_valid
-    expect(valid_user).to be_valid
-  end
+  # it 'validates presence of email' do
+    # expect(invalid_user).not_to be_valid
+    # expect(valid_user).to be_valid
+  # end
+
+  it { should validate_presence_of(:email) }
 
   it 'validates presence of password' do
     expect(no_password).not_to be_valid
   end
-
 end
--- Rails/Rails Auth/music_app/spec/rails_helper.rb
+++ Rails/Rails Auth/music_app/spec/rails_helper.rb
@@ -7,6 +7,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,3 +62,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
