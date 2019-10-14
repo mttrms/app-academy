9cb1925d8c0f8019ba8582e96f451f50be086145
matt <mattramos@gmail.com>
2019-10-13 22:57:54 -0400

    WIP users controller tests

--- Rails/Rails Testing/music_app/spec/controllers/users_controller_spec.rb
+++ Rails/Rails Testing/music_app/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
 
 end
