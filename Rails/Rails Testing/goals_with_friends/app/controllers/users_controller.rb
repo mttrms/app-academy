912839455f83cf0ea63fe24e7e646f9e198d871e
Matt Ramos <mattramos@gmail.com>
2019-10-15 10:54:33 -0400

    Write users controller

--- Rails/Rails Testing/goals_with_friends/app/controllers/users_controller.rb
+++ Rails/Rails Testing/goals_with_friends/app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
