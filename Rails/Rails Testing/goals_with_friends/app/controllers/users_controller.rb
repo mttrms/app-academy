b3e744c0647780099b5c5f9a0945be772189764f
Matt Ramos <mattramos@gmail.com>
2019-10-28 22:24:41 -0400

    Hide private goals

--- Rails/Rails Testing/goals_with_friends/app/controllers/users_controller.rb
+++ Rails/Rails Testing/goals_with_friends/app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @goals = current_user == @user ? @user.goals : @user.goals.where(private: false)
   end
 
   private
