c69e2745f23537371e0b9e1b7a1306484c90c32a
Matt Ramos <mattramos@gmail.com>
2019-11-09 15:26:45 -0500

    Posts can be created

--- Rails/Rails Forms/reddit_clone/app/controllers/posts_controller.rb
+++ Rails/Rails Forms/reddit_clone/app/controllers/posts_controller.rb
@@ -1,16 +1,49 @@
-class PostsController
+class PostsController < ApplicationController
+  before_action :validate_author, only: [:edit, :update]
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :content, :sub_id)
+  end
+
+  def validate_author
+    post = Post.find(params[:id])
+    return if current_user == post.author
+
+    redirect_to post_path(post)
   end
 end
