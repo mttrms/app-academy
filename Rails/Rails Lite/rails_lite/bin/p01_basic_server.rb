5fc84a559caf64586ff1fd55042c5906178cec07
Matt Ramos <mattramos@gmail.com>
2019-11-10 16:30:16 -0500

    Create simple Rack app

--- Rails/Rails Lite/rails_lite/bin/p01_basic_server.rb
+++ Rails/Rails Lite/rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,17 @@
 require 'rack'
+require 'pry'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write("<h1>Welcome to my Rack app!</h1>")
+  res.write("You are requesting: ")
+  res.write("<strong>#{req.path}</strong>")
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
