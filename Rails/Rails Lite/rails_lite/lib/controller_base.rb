908f910d4465cec6870d390f3890a987459f2735
Matt Ramos <mattramos@gmail.com>
2019-11-10 16:48:14 -0500

    Complete initialize and render_content methods

--- Rails/Rails Lite/rails_lite/lib/controller_base.rb
+++ Rails/Rails Lite/rails_lite/lib/controller_base.rb
@@ -8,10 +8,14 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
@@ -22,6 +26,11 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "DoubleRenderError" if already_built_response?
+
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
