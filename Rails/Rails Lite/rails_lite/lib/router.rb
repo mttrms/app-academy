e794c983268fbe64bafa275d3d34e8afbbc6bb6e
Matt Ramos <mattramos@gmail.com>
2019-11-10 22:13:55 -0500

    Define methods for each request type. Return matching route for request

--- Rails/Rails Lite/rails_lite/lib/router.rb
+++ Rails/Rails Lite/rails_lite/lib/router.rb
@@ -25,10 +25,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -39,10 +41,18 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
+
+    nil
   end
 
   # either throw 404 or call run on a matched route
