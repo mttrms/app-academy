05586b3958402fc8199b5c7b03d880b5d8d47b5a
Matt Ramos <mattramos@gmail.com>
2019-11-10 19:23:08 -0500

    Store session data with ControllerBase

--- Rails/Rails Lite/rails_lite/lib/controller_base.rb
+++ Rails/Rails Lite/rails_lite/lib/controller_base.rb
@@ -25,6 +25,8 @@ class ControllerBase
 
     @res.status = 302
     @res['Location'] = url
+    session.store_session(res)
+
     @already_built_response = true
   end
 
@@ -36,6 +38,8 @@ class ControllerBase
 
     @res['Content-Type'] = content_type
     @res.write(content)
+    session.store_session(res)
+
     @already_built_response = true
   end
 
@@ -54,6 +58,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
