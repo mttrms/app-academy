d8820f639b26f2d47e299855c030cc6ab2c24961
Matt Ramos <mattramos@gmail.com>
2019-11-10 21:22:58 -0500

    Initialize Route and add #match?

--- Rails/Rails Lite/rails_lite/lib/router.rb
+++ Rails/Rails Lite/rails_lite/lib/router.rb
@@ -1,11 +1,18 @@
+require 'pry'
+
 class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    pattern.match?(req.path) && http_method.to_s.upcase == req.request_method
   end
 
   # use pattern to pull out route params (save for later?)
