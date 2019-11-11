f34611378009b913d14b6b2e121d591a217dca5d
Matt Ramos <mattramos@gmail.com>
2019-11-11 00:38:28 -0500

    Enable Flash (POC)

--- Rails/Rails Lite/rails_lite/lib/flash.rb
+++ Rails/Rails Lite/rails_lite/lib/flash.rb
@@ -1,4 +1,34 @@
 require 'json'
+require 'pry'
 
 class Flash
+  attr_reader :now
+
+  def initialize(req)
+    @flash = {
+      flash_1: {},
+      flash_2: {}
+    }
+
+    @now = @flash[:flash_2]
+
+    if req.cookies['_rails_lite_app_flash']
+      @flash[:flash_2] = JSON.parse(req.cookies['_rails_lite_app_flash'])
+    end
+  end
+
+  def [](key)
+    flash_1 = @flash[:flash_1]
+    flash_2 = @flash[:flash_2]
+    all_flash = flash_2.merge(flash_1)
+    all_flash[key.to_s]
+  end
+
+  def []=(key, val)
+    @flash[:flash_1][key.to_s] = val
+  end
+
+  def store_flash(res)
+    res.set_cookie('_rails_lite_app_flash', {path: '/', value: @flash[:flash_1].to_json})
+  end
 end
