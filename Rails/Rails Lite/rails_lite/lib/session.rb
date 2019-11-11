483808147fadee17c44c44e526c589daea727ce2
Matt Ramos <mattramos@gmail.com>
2019-11-10 19:08:56 -0500

    Read and set cookies

--- Rails/Rails Lite/rails_lite/lib/session.rb
+++ Rails/Rails Lite/rails_lite/lib/session.rb
@@ -1,19 +1,27 @@
 require 'json'
+require 'pry'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @cookies = {}
+    if req.cookies["_rails_lite_app"]
+      @cookies = JSON.parse(req.cookies["_rails_lite_app"])
+    end
   end
 
   def [](key)
+    @cookies[key]
   end
 
   def []=(key, val)
+    @cookies[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie("_rails_lite_app", {path: "/", value: @cookies.to_json})
   end
 end
