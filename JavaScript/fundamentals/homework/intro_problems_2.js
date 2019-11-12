3352e4d440b6fc27521237202881841c74f3ae08
Matt Ramos <mattramos@gmail.com>
2019-11-12 12:58:46 -0500

    Constructors and prototypes

--- JavaScript/fundamentals/homework/intro_problems_2.js
+++ JavaScript/fundamentals/homework/intro_problems_2.js
@@ -11,3 +11,26 @@ const printCallback = (arr) => {
     console.log(element);
   })
 }
+
+function Elephant(name, height, tricksArr) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricksArr;
+}
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} the elephant goes woooooooo`);
+}
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+}
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function() {
+  trickIndex = Math.floor(Math.random() * this.tricks.length)
+  console.log(`${this.name} is ${this.tricks[trickIndex]}`)
+}
