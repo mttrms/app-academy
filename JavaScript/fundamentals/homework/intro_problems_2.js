df92c6b90d019ebe37503bccc85d83e7a798d0ef
Matt Ramos <mattramos@gmail.com>
2019-11-12 14:19:29 -0500

    Finish intro problem set 2 (closures)

--- JavaScript/fundamentals/homework/intro_problems_2.js
+++ JavaScript/fundamentals/homework/intro_problems_2.js
@@ -34,3 +34,32 @@ Elephant.prototype.play = function() {
   trickIndex = Math.floor(Math.random() * this.tricks.length)
   console.log(`${this.name} is ${this.tricks[trickIndex]}`)
 }
+
+Elephant.paradeHelper = function(elephant) {
+  console.log(`${elephant.name} is trotting by!`)
+}
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+herd.forEach(function(elephant) {
+  Elephant.paradeHelper(elephant);
+})
+
+const dinerBreakfast = () => {
+  let order = ['cheesy scrambled eggs'];
+  console.log(`I'd like ${order.join(' and ')} please.`);
+  
+  return function(food) {
+    order.push(food);
+    console.log(`I'd like ${order.join(' and ')} please.`);
+  }
+}
+
+let bfastOrder = dinerBreakfast();
+bfastOrder('chocolate chip pancakes');
+bfastOrder('grits');
