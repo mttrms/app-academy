c9ea3b1d2c41d9bfbe3b9feafbaae430ce1cd3e2
Matt Ramos <mattramos@gmail.com>
2019-11-15 17:54:58 -0500

    promptMove, print, stub out validateMove

--- JavaScript/event_loop/projects/towers_of_hanoi/game.js
+++ JavaScript/event_loop/projects/towers_of_hanoi/game.js
@@ -1,7 +1,34 @@
+// Remove later - but needed for debugging:
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 class Game {
   constructor(numOfDiscs) {
     this.towers = _makeTowers(numOfDiscs);
   }
+}
+
+Game.prototype.promptMove = function () {
+  this.print();
+  console.log('Pick towers to move a disc from -> to (Example: 1, 3)')
+  reader.question('Make your move: ', (response) => {
+    this.validateMove(response);
+  })
+}
+
+Game.prototype.print = function () {
+  this.towers.forEach((tower, idx) => {
+    console.log(`Tower ${idx}: ${JSON.stringify(tower)}`);
+  })
+}
+
+Game.prototype.validateMove = function(move) {
+  move = move.replace(/ /g,'').split(',');
+  console.log(`Start idx: ${move[0]}`);
+  console.log(`End idx: ${move[1]}`);
 }
 
 const _makeTowers = (numOfDiscs) => {
@@ -11,3 +38,6 @@ const _makeTowers = (numOfDiscs) => {
     []
   ]
 }
+
+const game = new Game(4);
+game.promptMove();
