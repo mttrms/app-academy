35e675ca72edb9efe6434711087d608ec1fb6f60
Matt Ramos <mattramos@gmail.com>
2019-11-16 01:08:24 -0500

    return values of cols, rows, diags

--- JavaScript/event_loop/projects/tic_tac_toe/board.js
+++ JavaScript/event_loop/projects/tic_tac_toe/board.js
@@ -1,6 +1,10 @@
 class Board {
   constructor() {
-    this._grid = [[], [], []]
+    this._grid = [
+      new Array(3).fill('_'),
+      new Array(3).fill('_'),
+      new Array(3).fill('_')
+    ]
   }
 
   winner() {
@@ -8,13 +12,12 @@ class Board {
   }
 
   won() {
-
   }
 
   empty(pos) {
     const [row, col] = pos;
 
-    return this._grid[row][col] === undefined;
+    return this._grid[row][col] === '_';
   }
 
   valid(pos) {
@@ -29,9 +32,32 @@ class Board {
       throw Error('Invalid placement.')
     }
   }
+
+  _rows(){ 
+    return this._grid;
+  }
+
+  _cols() {
+    const cols = [[], [], []]
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        cols[j].push(this._grid[i][j])
+      }
+    }
+
+    return cols;
+  }
+
+  _diagonals() {
+    const diagonal_down = [[0, 0], [1, 1], [2, 2]];
+    const diagonal_up = [[2, 0], [1, 1], [0, 2]];
+    const diagonals = [diagonal_down, diagonal_up];
+
+    return diagonals.map((diagonal) => {
+      return diagonal.map((space) => {
+        return this._grid[space[0]][space[1]];
+      })
+    })
+  }
 }
-
-
-const board = new Board
-// console.log(board.empty([0,0]))
-console.log(board.valid([0, 0]))
