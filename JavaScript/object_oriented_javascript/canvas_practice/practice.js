776e54af9e2a189cd0fd6729641d13aa65014f79
Matt Ramos <mattramos@gmail.com>
2019-11-17 01:18:27 -0500

    Draw some shapes

--- JavaScript/object_oriented_javascript/canvas_practice/practice.js
+++ JavaScript/object_oriented_javascript/canvas_practice/practice.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
+	const canvas = document.getElementById('mycanvas');
+	const ctx = canvas.getContext('2d');
 
+	ctx.fillStyle = 'mistyrose';
+	ctx.fillRect(50, 50, 400, 200);
+
+	ctx.beginPath();
+	ctx.arc(200, 200, 25, 0, 2 * Math.PI);
+	ctx.strokeStyle = 'darkseagreen';
+	ctx.lineWidth = 10;
+	ctx.stroke();
+	ctx.fillStyle = 'palegreen';
+	ctx.fill();
 });
