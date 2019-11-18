b0e23a9fd2de36467d0ebb9c0847fac09f92f9b1
Matt Ramos <mattramos@gmail.com>
2019-11-17 22:46:56 -0500

    Add draw and moveObjects to Game class

--- JavaScript/object_oriented_javascript/asteroids/src/game.js
+++ JavaScript/object_oriented_javascript/asteroids/src/game.js
@@ -19,6 +19,19 @@ Game.prototype.randomPosition = function() {
   return [random_x, random_y]
 }
 
+Game.prototype.draw = function(ctx) {
+  ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+  this.asteroids.forEach((asteroid) => {
+    asteroid.draw(ctx);
+  })
+}
+
+Game.prototype.moveObjects = function() {
+  this.asteroids.forEach((asteroid) => {
+    asteroid.move();
+  })
+}
+
 Game.DIM_X = 1200;
 Game.DIM_Y = 800;
 Game.NUM_ASTEROIDS = 25;
