a646f8eb75435d30ae9a267b80466309816fa116
Matt Ramos <mattramos@gmail.com>
2019-11-25 23:36:58 -0500

    Render a board

--- JavaScript/jquery/tic_tac_toe/src/ttt-view.js
+++ JavaScript/jquery/tic_tac_toe/src/ttt-view.js
@@ -1,11 +1,25 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const $ul = $("<ul><li></li></ul>")
+    for (let i = 0; i < 8; i++) {
+      const $li = $("<li></li>")
+      $ul.append($li);
+    }
+
+    this.$el.append($ul)
+
+    console.log($ul);
+  }
 }
 
 module.exports = View;
