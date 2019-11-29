79681f4e13d6211dd550a353cc355fb8c393a91e
matt <mattramos@gmail.com>
2019-11-29 15:30:34 -0500

    Get the weather

--- JavaScript/ajax/ajax_weather/js/index.js
+++ JavaScript/ajax/ajax_weather/js/index.js
@@ -1,5 +1,12 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
+$.ajax({
+	method: "GET",
+	url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b&units=imperial",
+	dataType: "JSON",
+	success: function(r) {
+		console.log(`Temp: ${r.main.temp}. Wind: ${r.wind.speed} mph. It is ${r.weather[0].main}.`);
+	}
+});
 
-// Add another console log here, outside your AJAX request
+console.log("Weather req sent.");
