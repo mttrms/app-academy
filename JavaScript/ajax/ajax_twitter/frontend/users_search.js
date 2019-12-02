90899895cd6e40fb6cf0629c5597704ebc8576e9
Matt Ramos <mattramos@gmail.com>
2019-12-01 20:04:27 -0500

    Render user results

--- JavaScript/ajax/ajax_twitter/frontend/users_search.js
+++ JavaScript/ajax/ajax_twitter/frontend/users_search.js
@@ -12,10 +12,23 @@ class UsersSearch {
 		this.$input.on('input', function(e) {
 			const query = e.target.value;
 			APIUtil.searchUsers(query)
-				.then((res) => {
-					console.log(res)
-				})
+				.then(res => {
+					this.renderResults(res);
+				});
+		}.bind(this));
+	}
+
+	renderResults(res) {
+		this.$ul.empty();
+
+		res.forEach(user => {
+			const li = $('<li>');
+			li.text(user.username);
+			this.$ul.append(li);
+			console.log(li);
 		})
+
+		console.log(res);
 	}
 }
 
