de38b5969f5fa764a7ce0c7984ca02582ae59f16
Matt Ramos <mattramos@gmail.com>
2019-12-05 23:46:45 -0500

    Append list elements

--- JavaScript/dom_api/vanilla_dom_manipulation/script.js
+++ JavaScript/dom_api/vanilla_dom_manipulation/script.js
@@ -14,18 +14,17 @@ document.addEventListener("DOMContentLoaded", () => {
     li.addEventListener("click", toggleLi);
   });
 
+  const submitButton = document.querySelector('.list-container form');
 
+  submitButton.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  // adding SF places as list items
+    const favoritePlace = document.querySelector('.favorite-input');
+    const ul = document.getElementById('sf-places')
+    const li = document.createElement("li");
 
-  // --- your code here!
-
-
-
-  // adding new photos
-
-  // --- your code here!
-
-
-
+    li.textContent = favoritePlace.value;
+    ul.appendChild(li);
+    favoritePlace.value = '';
+  })
 });
