1b0ee65796a3cfd4c11f67d7311b642676704bc9
Matt Ramos <mattramos@gmail.com>
2019-12-07 16:13:40 -0500

    Add html and empty functions, append WIP

--- JavaScript/dom_api/jquery_lite/src/dom_node_collection.js
+++ JavaScript/dom_api/jquery_lite/src/dom_node_collection.js
@@ -2,6 +2,31 @@ class DOMNodeCollection {
   constructor(htmlElementArr) {
     this.elements = htmlElementArr;
   }
+
+  html(string) {
+    if (string !== undefined) {
+      this.elements.forEach(element => element.innerHTML = string);
+    } else {
+      return this.elements[0].innerHTML;
+    }
+  }
+
+  empty() {
+    this.html('');
+  }
+
+  append(children) {
+    if (typeof children === 'object' && !(children instanceof DOMNodeCollection)) {
+      children = $l(children);
+    }
+
+    if (typeof children === 'string') {
+      this.elements.forEach(element => element.innerHTML += children);
+    } else if (children instanceof DOMNodeCollection) {
+      this.elements.forEach((element) => {
+      })
+    }
+  }
 }
 
 module.exports = DOMNodeCollection;
