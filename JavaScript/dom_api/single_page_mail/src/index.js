9f0bc62810611974b885796b57bef85bc989bb33
Matt Ramos <mattramos@gmail.com>
2019-12-09 12:53:11 -0500

    Use router to update DOM

--- JavaScript/dom_api/single_page_mail/src/index.js
+++ JavaScript/dom_api/single_page_mail/src/index.js
@@ -1,5 +1,10 @@
+const Router = require('./router.js');
+
 document.addEventListener("DOMContentLoaded", () => {
   const sidebarNavLinks = document.querySelectorAll('.sidebar-nav li');
+  const node = document.getElementsByClassName('content')[0];
+  const router = new Router(node);
+  router.start();
 
   sidebarNavLinks.forEach((element) => {
     element.addEventListener('click', () => {
--- JavaScript/dom_api/single_page_mail/src/router.js
+++ JavaScript/dom_api/single_page_mail/src/router.js
@@ -2,4 +2,25 @@ class Router {
   constructor(node) {
     this.node = node;
   }
+
+  start() {
+    this.render();
+
+    window.addEventListener('hashchange', () => {
+      this.render();
+    })
+  }
+
+  render() {
+    this.node.innerHTML = '';
+    const p = document.createElement('p');
+    p.innerHTML = this.activeRoute();
+    this.node.appendChild(p);
+  }
+
+  activeRoute() {
+    return window.location.hash.substring(1);
+  }
 }
+
+module.exports = Router;
