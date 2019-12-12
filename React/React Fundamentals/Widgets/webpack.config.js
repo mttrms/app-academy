69e1dec60109f910ab704fb1c2b559c723233175
Matt Ramos <mattramos@gmail.com>
2019-12-11 23:22:26 -0500

    Initialize widgets project

---
+++ React/React Fundamentals/Widgets/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './widgets.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+}
