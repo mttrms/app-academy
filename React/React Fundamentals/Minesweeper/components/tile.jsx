9ef312f56478a311eab193d4024ddc6ae54eb129
Matt Ramos <mattramos@gmail.com>
2019-12-14 21:05:25 -0500

    Stub out a Tile component

---
+++ React/React Fundamentals/Minesweeper/components/tile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <span>T</span>
+    )
+  }
+}
+
+export default Tile;
