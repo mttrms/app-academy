07f7e3dc8519a871f87f4ed41357218a6a717e29
Matt Ramos <mattramos@gmail.com>
2019-12-14 23:05:59 -0500

    Add appropriate class names to Tiles

--- React/React Fundamentals/Minesweeper/components/tile.jsx
+++ React/React Fundamentals/Minesweeper/components/tile.jsx
@@ -3,11 +3,35 @@ import React from 'react';
 class Tile extends React.Component {
   constructor(props) {
     super(props);
+    this.tile = this.props.tileObject;
+    this.classes = this.classes.bind(this);
+  }
+
+  classes() {
+    const classList = {
+      tile: true,
+      explored: this.tile.explored,
+      hidden: !this.tile.explored,
+      flagged: this.tile.flagged
+    }
+
+    return Object.keys(classList).reduce((arr, className) => {
+      if (classList[className]) {
+        arr.push(className);
+      }
+
+      return arr;
+    }, [])
   }
 
   render() {
+
     return(
-      <span>T</span>
+      <div
+        className={ this.classes().join(' ') }
+      >
+        {this.tile.explored ? this.tile.adjacentBombCount() : ""}
+      </div>
     )
   }
 }
