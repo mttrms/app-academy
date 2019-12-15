4124e145d1081467f4a6aa75e3be35d04f8e070b
Matt Ramos <mattramos@gmail.com>
2019-12-14 23:37:28 -0500

    Add click events for tiles and check for altKey presses

--- React/React Fundamentals/Minesweeper/components/tile.jsx
+++ React/React Fundamentals/Minesweeper/components/tile.jsx
@@ -4,7 +4,14 @@ class Tile extends React.Component {
   constructor(props) {
     super(props);
     this.tile = this.props.tileObject;
+    this.updateGame = this.props.updateGame;
+    this.handleClick = this.handleClick.bind(this);
     this.classes = this.classes.bind(this);
+  }
+
+  handleClick(e) {
+    isAltPushed = e.altKey;
+    this.updateGame(this.tile, isAltPushed);
   }
 
   classes() {
@@ -29,6 +36,7 @@ class Tile extends React.Component {
     return(
       <div
         className={ this.classes().join(' ') }
+        onClick={ this.handleClick }
       >
         {this.tile.explored ? this.tile.adjacentBombCount() : ""}
       </div>
