2be69b5fa2c32fb1ba9d94aa8373b700b8d35ee8
Matt Ramos <mattramos@gmail.com>
2019-12-16 22:44:39 -0500

    WIP - combineReducers

---
+++ React/React Fundamentals/Redux Lite/combine-reducers.js
@@ -0,0 +1,41 @@
+const combineReducers = (reducers) => {
+
+}
+
+const myNoiseReducer = (prevState = "peace and quiet", action) => {
+  switch(action.type) {
+    case "noisy action":
+      return action.noise;
+    default:
+      return prevState;
+  }
+};
+
+const myNoisyAction = {
+  type: "noisy action",
+  noise: "Car alarm"
+};
+
+const myInconsequentialAction = {
+  type: "a type no one cares about",
+  data: {
+    thisThing: "will not get used anyway"
+  }
+};
+
+const myInitialState = {
+  noise: "peace and quiet"
+};
+
+const myRootReducer = combineReducers({
+  noise: myNoiseReducer,
+});
+
+let newState = myRootReducer(myInitialState, myInconsequentialAction);
+// => { noise: "peace and quiet" }
+
+newState = myRootReducer(newState, myNoisyAction)
+// => { noise: "Car alarm" }
+
+myRootReducer(newState, myInconsequentialAction)
+// => { noise: "Car alarm" }
