c392d183d81a49e50bfe88f048cfa5208848d4b2
Matt Ramos <mattramos@gmail.com>
2019-12-17 00:31:29 -0500

    Add a dispatch function

--- React/React Fundamentals/Redux Lite/store.js
+++ React/React Fundamentals/Redux Lite/store.js
@@ -7,6 +7,25 @@ class Store {
   getState() {
     return Object.assign(this.globalState);
   }
+
+  dispatch(action) {
+    const rootReducer = this.rootReducer;
+    const prevState = this.globalState;
+    const nextState = rootReducer(prevState, action);
+
+    this.globalState = nextState;
+  }
 }
 
-export default Store;
+const combineReducers = (reducers) => {
+  return (prevState, action) => {
+    const newState = {};
+
+    Object.keys(reducers).forEach(key => {
+      newState[key] = reducers[key](prevState[key], action);
+    })
+
+    return newState;
+  }
+}
+
