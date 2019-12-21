520d5a47d6824e6ed2bf9c36dc2b1e49df80ecae
Matt Ramos <mattramos@gmail.com>
2019-12-21 15:53:11 -0500

    Write Action Creators for todos

---
+++ React/Redux Fundamentals/ReduxTodos/frontend/actions/todo_actions.js
@@ -0,0 +1,12 @@
+export const RECEIVE_TODOS = 'RECEIVE_TODOS';
+export const RECEIVE_TODO = 'RECEIVE_TODO';
+
+export const receiveTodos = (todos) => ({
+  type: RECEIVE_TODOS,
+  todos: todos
+});
+
+export const receiveTodo = (todo) => ({
+  type: RECEIVE_TODO,
+  todo: todo
+})
--- React/Redux Fundamentals/ReduxTodos/todo_redux.jsx
+++ React/Redux Fundamentals/ReduxTodos/todo_redux.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveTodos, RECEIVE_TODOS } from './frontend/actions/todo_actions.js';
 
 const TodoApp = () => (
   <h1>Todos App</h1>
