ec4df1642a77a1d8fc437f9570b1f3a0e574e673
Matt Ramos <mattramos@gmail.com>
2019-12-21 15:35:42 -0500

    Create + render entry component

--- React/Redux Fundamentals/ReduxTodos/todo_redux.jsx
+++ React/Redux Fundamentals/ReduxTodos/todo_redux.jsx
@@ -1,1 +1,13 @@
-console.log('Initializiong Todo App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoApp = () => (
+  <h1>Todos App</h1>
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('content')
+  );
+});
