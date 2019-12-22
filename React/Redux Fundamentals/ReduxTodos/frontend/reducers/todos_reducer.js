560d28879a8d449e8ccd435286548962969d514d
Matt Ramos <mattramos@gmail.com>
2019-12-22 18:20:58 -0500

    Set initial state

--- React/Redux Fundamentals/ReduxTodos/frontend/reducers/todos_reducer.js
+++ React/Redux Fundamentals/ReduxTodos/frontend/reducers/todos_reducer.js
@@ -1,6 +1,21 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
   switch(action.type) {
     case RECEIVE_TODOS:
       return action.todos.reduce((state, todo) => {
