2c779609234e45f268f72d653a3b3ace22848f3f
Matt Ramos <mattramos@gmail.com>
2019-12-28 23:44:40 -0500

    Add initial steps reducer

--- React/Redux Fundamentals/ReduxTodos/frontend/reducers/root_reducer.js
+++ React/Redux Fundamentals/ReduxTodos/frontend/reducers/root_reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import todosReducer from './todos_reducer';
+import stepsReducer from './steps_reducer';
 import { allTodos } from './selectors';
 
 window.allTodos = allTodos;
 
 const rootReducer = combineReducers({
-  todos: todosReducer
+  todos: todosReducer,
+  steps: stepsReducer
 })
 
 export default rootReducer;
---
+++ React/Redux Fundamentals/ReduxTodos/frontend/reducers/steps_reducer.js
@@ -0,0 +1,10 @@
+import { RECEIVE_STEPS, RECEIVE_STEP, REMOVE_STEP } from '../actions/step_actions';
+
+const stepsReducer = (state = {}, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default stepsReducer;
