bc9fee5b1dc2c758550630084d8026475fe8e8da
Matt Ramos <mattramos@gmail.com>
2019-12-30 13:27:55 -0500

    Create a step list container

--- React/Redux Fundamentals/ReduxTodos/frontend/components/todos/todo_detail_view_container.jsx
+++ React/Redux Fundamentals/ReduxTodos/frontend/components/todos/todo_detail_view_container.jsx
@@ -2,7 +2,8 @@ import { connect } from 'react-redux';
 import TodoDetailView from 'todo-detail-view';
 
 const mapDispatchToProps = dispatch => ({
-  removeTodo: todo => dispatch(removeTodo(todo))
+  removeTodo: todo => dispatch(removeTodo(todo)),
+  receiveSteps: steps => dispatch(receiveSteps(steps))
 });
 
 export default connect(
