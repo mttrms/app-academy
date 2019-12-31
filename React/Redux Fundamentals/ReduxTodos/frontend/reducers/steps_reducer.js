c29d9158005895fbbe8f46b0b3341012ca184150
Matt Ramos <mattramos@gmail.com>
2019-12-31 17:51:35 -0500

    Create steps reducer

--- React/Redux Fundamentals/ReduxTodos/frontend/reducers/steps_reducer.js
+++ React/Redux Fundamentals/ReduxTodos/frontend/reducers/steps_reducer.js
@@ -2,9 +2,30 @@ import { RECEIVE_STEPS, RECEIVE_STEP, REMOVE_STEP } from '../actions/step_action
 
 const stepsReducer = (state = {}, action) => {
   switch(action.type) {
+    case RECEIVE_STEPS: {
+      const newState = Object.assign({}, state);
+      action.steps.forEach((step) => {
+        newState[step.id] = step;
+      })
+
+      return newState;
+    }
+    case RECEIVE_STEP: {
+      const newState = Object.assign({}, state);
+      newState[action.step.id] = action.step;
+
+      return newState;
+    }
+    case REMOVE_STEP: {
+      const newState = Object.assign({}, state);
+      delete newState[action.step.id];
+
+      return newState;
+    }
     default:
       return state;
   }
 };
 
 export default stepsReducer;
+
