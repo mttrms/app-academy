29623f003b5668fd5b9169c31439ffc4eea550a0
Matt Ramos <mattramos@gmail.com>
2020-01-02 23:06:14 -0500

    Add button to complete/uncomplete steps

--- React/Redux Fundamentals/ReduxTodos/frontend/components/step_list/step_list_item.jsx
+++ React/Redux Fundamentals/ReduxTodos/frontend/components/step_list/step_list_item.jsx
@@ -3,12 +3,30 @@ import React from 'react';
 class StepListItem extends React.Component {
   constructor(props) {
     super(props);
-    this.step = this.props.step;
+    this.receiveStep = this.props.receiveStep;
+    this.updateStep = this.updateStep.bind(this);
+  }
+
+  updateStep(step) {
+    const updatedStep = Object.assign({}, step);
+    updatedStep.done = !updatedStep.done;
+    this.receiveStep(updatedStep);
   }
 
   render() {
+    const step = this.props.step;
+
     return (
-      <li>{this.step.title}</li>
+      <li>
+        <span>
+          {step.title}
+        </span>
+        <input
+          type="button"
+          value={step.done ? "Undo" : "Done"}
+          onClick={() => this.updateStep(step)}
+        />
+      </li>
     )
   }
 }
