24db39f0e8ac705bf70e6651103e172c0332697c
matt <mattramos@gmail.com>
2020-01-04 01:21:14 -0500

    Add logger middleware

--- React/Redux Fundamentals/ReduxTodos/frontend/store/store.js
+++ React/Redux Fundamentals/ReduxTodos/frontend/store/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger';
 
 export const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(
+    rootReducer,
+    {},
+    applyMiddleware(logger)
+  );
 };
