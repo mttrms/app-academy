ef104922b4532bf27e8cc7bce2f3f32f1e9b0fd0
matt <mattramos@gmail.com>
2020-01-04 13:17:54 -0500

    Add customer state logger

--- React/Redux Fundamentals/ReduxTodos/todo_redux.jsx
+++ React/Redux Fundamentals/ReduxTodos/todo_redux.jsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import Root from './frontend/components/root';
 import { configureStore } from './frontend/store/store';
 
+const addLoggingToDispatch = (store) => {
+  const dispatch = store.dispatch;
+
+  return function(action) {
+    console.log(store.getState());
+    console.log(action);
+    dispatch(action);
+    console.log(store.getState());
+  };
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('content');
 
   const store = configureStore();
+  store.dispatch = addLoggingToDispatch(store);
   window.store = store;
 
   ReactDOM.render(
