43096f0d2522ea19dfb7d8836927011c6be53cec
Matt Ramos <mattramos@gmail.com>
2020-01-12 22:16:45 -0500

    Add Steps controller and routes

---
+++ React/Middleware and Thunks/Todos/todoredux/app/controllers/api/steps_controller.rb
@@ -0,0 +1,44 @@
+class Api::StepsController < ApplicationController
+  def show
+    @step = Step.find(params[:id])
+    render json: @step
+  end
+
+  def index
+    @steps = Step.all
+    render json: @steps
+  end
+
+  def create
+    @step = Step.new(step_params)
+    if @step.save
+      render json: @step
+    else
+      render json: @step.errors.full_messages, status: :bad_request
+    end
+  end
+
+  def update
+    @step = Step.find(params[:id])
+    if @step.update(step_params)
+      render json: @step
+    else
+      render json: @step.errors.full_messages, status: :bad_request
+    end
+  end
+
+  def destroy
+    @step = Step.find(params[:id])
+    if @step.destroy
+      render json: 'Delete successful.'
+    else
+      render json: @step
+    end
+  end
+
+  private
+
+  def step_params
+    params.require(:step).permit(:title, :body, :done, :todo_id)
+  end
+end
--- React/Middleware and Thunks/Todos/todoredux/config/routes.rb
+++ React/Middleware and Thunks/Todos/todoredux/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  namespace :api do
-    resources :todos,
-      only: [:show, :index, :create, :update, :destroy],
-      defaults: { format: :json }
+  namespace :api, defaults: { format: :json } do
+    resources :todos, only: [:show, :index, :create, :update, :destroy] do
+      resources :steps, only: [:index, :create]
+    end
+
+    resources :steps, only: [:update, :destroy]
   end
 end
