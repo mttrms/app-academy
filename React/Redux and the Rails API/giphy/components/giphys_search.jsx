5b2b892275588d8e0b2503d0f411243b984c200e
Matt Ramos <mattramos@gmail.com>
2020-01-15 00:46:51 -0500

    Create GiphysSearch component

--- React/Redux and the Rails API/giphy/components/giphys_search.jsx
+++ React/Redux and the Rails API/giphy/components/giphys_search.jsx
@@ -1,3 +1,21 @@
 import React from 'react';
-
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchTerm: ''};
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="searchterm">Find gifs:</label>
+        <input type="text" id="searchterm" />
+        <button>Search Giphy</button>
+      </form>
+    )
+  }
+}
+
+export default GiphysSearch;
