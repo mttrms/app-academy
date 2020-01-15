58c1ba88cd73955b6a1aaa0bdb620dff9f3e7676
Matt Ramos <mattramos@gmail.com>
2020-01-15 01:00:20 -0500

    Add search form

--- React/Redux and the Rails API/giphy/components/giphys_search.jsx
+++ React/Redux and the Rails API/giphy/components/giphys_search.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import GiphysIndex from './giphys_index';
 
 class GiphysSearch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {searchTerm: ''};
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(e) {
+    const searchTerm = e.target.value;
+    this.setState({searchTerm});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const searchTerm = this.state.searchTerm;
+    this.props.fetchSearchGiphys(searchTerm);
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={ this.handleSubmit }>
         <label htmlFor="searchterm">Find gifs:</label>
-        <input type="text" id="searchterm" />
+        <input
+          className="input"
+          value={ this.state.searchTerm }
+          onChange={ this.handleInput }
+          type="text"
+          id="searchterm"
+        />
         <button>Search Giphy</button>
       </form>
     )
   }
+}
+
+GiphysSearch.propTypes = {
+  fetchSearchGiphys: PropTypes.func
 }
 
 export default GiphysSearch;
