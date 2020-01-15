53b3fb76fdfa8c628b816528ae0cdb4cd3d26f08
Matt Ramos <mattramos@gmail.com>
2020-01-14 23:40:59 -0500

    Cleanup entry file

--- React/Redux and the Rails API/giphy/giphy_search.jsx
+++ React/Redux and the Rails API/giphy/giphy_search.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { configureStore } from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './util/api_util';
-import { receiveSearchGiphys } from './actions/giphy_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('root');
