4d779aed17fbf175cf20d475f9928e4e4810bf43
Matt Ramos <mattramos@gmail.com>
2020-01-16 00:04:02 -0500

    Add routes - complete guests#show views

--- React/Redux and the Rails API/jbuilder/config/routes.rb
+++ React/Redux and the Rails API/jbuilder/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # Your routes here!
+  namespace :api, defaults: { format: :json } do
+    resources :guests, only: [:show, :index] do
+      resources :gifts, only: [:index]
+    end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :gifts, only: [:show]
+    resources :parties, only: [:show]
+  end
 end
