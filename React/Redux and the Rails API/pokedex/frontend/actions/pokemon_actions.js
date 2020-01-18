2602f0dc81cfc072383e542c7f46b7e7db7e1fd2
Matt Ramos <mattramos@gmail.com>
2020-01-17 23:04:35 -0500

    Add receiveAllPokemon action creator

---
+++ React/Redux and the Rails API/pokedex/frontend/actions/pokemon_actions.js
@@ -0,0 +1,8 @@
+import * as APIUtil from '../util/api_util';
+
+export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
+
+export const receiveAllPokemon = pokemon => ({
+  type: RECEIVE_ALL_POKEMON,
+  pokemon
+});
