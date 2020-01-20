8b118e161fafc5952a43e030dcd5856b0bb60c77
Matt Ramos <mattramos@gmail.com>
2020-01-19 23:30:25 -0500

    Get pokemon on mount

--- React/Redux and the Rails API/pokedex/frontend/components/pokemon/pokemon_index.jsx
+++ React/Redux and the Rails API/pokedex/frontend/components/pokemon/pokemon_index.jsx
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import PokemonIndexContainer from './pokemon_index_container';
 
 class PokemonIndex extends React.Component {
+  componentDidMount() {
+    this.props.requestAllPokemon();
+  }
+
   render() {
     return (
       <p>I am a poke</p>
--- React/Redux and the Rails API/pokedex/frontend/components/pokemon/pokemon_index_container.js
+++ React/Redux and the Rails API/pokedex/frontend/components/pokemon/pokemon_index_container.js
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
+
 import PokemonIndex from './pokemon_index';
 import { requestAllPokemon } from '../../actions/pokemon_actions';
+import { selectAllPokemon } from '../../reducers/selectors';
 
 const mapStateToProps = state => ({
-  pokemon: state.entities.pokemon,
+  pokemon: selectAllPokemon(state),
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestAllPokemon: dispatch(requestAllPokemon),
+  requestAllPokemon: () => dispatch(requestAllPokemon()),
 });
 
 export default connect(
