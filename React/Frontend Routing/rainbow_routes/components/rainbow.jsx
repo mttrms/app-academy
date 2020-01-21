bdc72cc3ef804725a52d261f63a92695fdef926f
Matt Ramos <mattramos@gmail.com>
2020-01-20 19:29:27 -0500

    Setup initial Routes

--- React/Frontend Routing/rainbow_routes/components/rainbow.jsx
+++ React/Frontend Routing/rainbow_routes/components/rainbow.jsx
@@ -18,6 +18,10 @@ class Rainbow extends React.Component {
         {/* Your links should go here */}
 
         <div id="rainbow">
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
           {/* Your routes should go here */}
         </div>
       </div>
