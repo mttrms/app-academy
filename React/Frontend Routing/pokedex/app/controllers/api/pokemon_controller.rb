4d5347fed856172f1e081d212eb3a350924210af
Matt Ramos <mattramos@gmail.com>
2020-01-22 21:42:28 -0500

    Enable POST requests

--- React/Frontend Routing/pokedex/app/controllers/api/pokemon_controller.rb
+++ React/Frontend Routing/pokedex/app/controllers/api/pokemon_controller.rb
@@ -1,4 +1,6 @@
 class Api::PokemonController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @pokemons = Pokemon.all
     render :index
@@ -7,5 +9,27 @@ class Api::PokemonController < ApplicationController
   def show
     @pokemon = Pokemon.find(params[:id])
     render :show
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    if @pokemon.save
+      render json: @pokemon
+    else
+      render json: 'error'
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(
+      :name,
+      :attack,
+      :defense,
+      :poke_type,
+      :moves,
+      :image_url
+    )
   end
 end
