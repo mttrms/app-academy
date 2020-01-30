1a30fcfa82c6ba0fd50c68ed3ec86bd1aa2ad476
Matt Ramos <mattramos@gmail.com>
2020-01-29 22:08:30 -0500

    Add session API util

---
+++ React/Frontend Auth/frontend_auth_project/frontend/utils/session.js
@@ -0,0 +1,22 @@
+export const postUser = user => (
+  $.ajax({
+    url: '/api/users',
+    method: 'POST',
+    data: { user },
+  })
+);
+
+export const postSession = user => (
+  $.ajax({
+    url: '/api/session',
+    method: 'POST',
+    data: { user },
+  })
+);
+
+export const deleteSession = () => (
+  $.ajax({
+    url: '/api/session',
+    method: 'DELETE',
+  })
+);
