1609b10cfee51a226bc7bdfba7f371544c6ebfe6
Matt Ramos <mattramos@gmail.com>
2020-01-29 22:31:04 -0500

    Add session actions

---
+++ React/Frontend Auth/frontend_auth_project/frontend/actions/session.js
@@ -0,0 +1,22 @@
+import { postUser, postSession, deleteSession } from '../util/session';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const createNewUser = formUser => dispatch => postUser(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => postSession(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => deleteSession()
+  .then(() => dispatch(logoutCurrentUser()));
