45ee045b35ddfe911f7f43f8d39f56e3ae54a076
Matt Ramos <mattramos@gmail.com>
2020-01-31 23:03:24 -0500

    Update form component

--- React/Frontend Auth/frontend_auth_project/frontend/components/session/signup.jsx
+++ React/Frontend Auth/frontend_auth_project/frontend/components/session/signup.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(type) {
+    return (e) => {
+      this.setState({ [type]: e.target.value });
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createNewUser(this.state)
+      .then(() => this.props.history.push('/chirps'));
+  }
+
   render () {
     return(
-      <div></div>
+      <div className="session-form">
+        <h2>Signup</h2>
+        <form>
+          <label>Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleInput('username')}
+            />
+          </label>
+          
+          <label>Email:
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInput('email')}
+            />
+          </label>
+
+          <label>Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInput('password')}
+            />
+          </label>
+          <button onClick={this.handleSubmit}>Signup</button>
+        </form>
+      </div>
     );
   }
 }
