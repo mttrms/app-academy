91e0e2588465de897cb988d61003327bd129f35d
Matt Ramos <mattramos@gmail.com>
2020-01-31 22:47:13 -0500

    Add signup and signup container

---
+++ React/Frontend Auth/frontend_auth_project/frontend/components/session/signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  render () {
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default Signup;
---
+++ React/Frontend Auth/frontend_auth_project/frontend/components/session/signup_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { createNewUser } from '../../actions/session';
+import Signup from './signup';
+
+const mapDispatchToProps = dispatch => ({
+  createNewUser: formUser => dispatch(createNewUser(formUser)),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Signup);
