d60b4b2adccdb5893bee1b76207bdcc69ec838cc
Matt Ramos <mattramos@gmail.com>
2020-02-07 00:41:17 -0500

    Add Annotate gem

--- React/Frontend Auth/benchbnb/app/models/user.rb
+++ React/Frontend Auth/benchbnb/app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password_digest, :session_token, presence: true
