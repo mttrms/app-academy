7ac1c53b4e8aeef98d30bdfcc814bec6153e2e92
Matt Ramos <mattramos@gmail.com>
2020-02-07 00:39:13 -0500

    Add User model

---
+++ React/Frontend Auth/benchbnb/app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest) == password
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+
+    self.session_token
+  end
+
+  attr_reader :password
+end
