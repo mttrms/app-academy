248f89761c13f389be0f1f11a06f5d45cdb62da7
Matt Ramos <mattramos@gmail.com>
2020-02-08 11:15:30 -0500

    Ensure session tokens

--- React/Frontend Auth/benchbnb/app/models/user.rb
+++ React/Frontend Auth/benchbnb/app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   validates :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
+  after_initialize :ensure_session_token
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user
