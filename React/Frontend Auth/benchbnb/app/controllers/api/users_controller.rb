b9c871ca94ad70477971b081e3732c977b037241
Matt Ramos <mattramos@gmail.com>
2020-02-09 15:41:54 -0500

    UsersController

--- React/Frontend Auth/benchbnb/app/controllers/api/users_controller.rb
+++ React/Frontend Auth/benchbnb/app/controllers/api/users_controller.rb
@@ -1,8 +1,4 @@
 class Api::UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def show
     @user = User.find(params[:id])
     render :show
@@ -12,24 +8,6 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       render :show
-    else
-      render json: @user.errors.full_messages
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      render :show
-    else
-      render json: @user.errors.full_messages
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    if @user.destroy
-      render json: 'Deleted'
     else
       render json: @user.errors.full_messages
     end
--- React/Frontend Auth/benchbnb/config/routes.rb
+++ React/Frontend Auth/benchbnb/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
   namespace :api, defaults: { format: :json } do
-    resources :users
+    resource :user, only: :create
   end
 end
