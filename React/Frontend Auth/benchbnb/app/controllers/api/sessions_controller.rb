e4f82cf23c79d22ce9f2569fc3e15fc507b14a19
Matt Ramos <mattramos@gmail.com>
2020-02-09 17:08:16 -0500

    Add session logic

---
+++ React/Frontend Auth/benchbnb/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user],
+      params[:password]
+    )
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ['Invalid credentials'], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      @user.reset_session_token!
+      session[:session_token] = nil
+      render 'api/users/show'
+    else
+      render json: ['User is not signed in'], status: 404
+    end
+  end
+end
--- React/Frontend Auth/benchbnb/app/controllers/application_controller.rb
+++ React/Frontend Auth/benchbnb/app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+
+  def current_user
+    return nil unless session[:session_token]
+
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
 end
