cf18b5f3accb7335fc1296925e0c595123a3e94e
Matt Ramos <mattramos@gmail.com>
2020-02-08 22:05:24 -0500

    WIP User API

---
+++ React/Frontend Auth/benchbnb/app/controllers/api/users_controller.rb
@@ -0,0 +1,43 @@
+class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: 'Deleted'
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- React/Frontend Auth/benchbnb/config/routes.rb
+++ React/Frontend Auth/benchbnb/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
 end
