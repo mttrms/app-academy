b31fc5e8961577093aeb258de943e9030efb2d05
Matt Ramos <mattramos@gmail.com>
2020-02-14 17:29:24 -0500

    WIP session actions

---
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/actions/session_actions.js
@@ -0,0 +1,5 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
