735677624e07a1f2058da2f2c6af27da6d1a26b2
Matt Ramos <mattramos@gmail.com>
2020-02-16 15:22:18 -0500

    Add session form component

---
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/session_form/session_form.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTargert.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault;
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            { error }
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.update('username')}
+            />
+          </label>
+          <br />
+          <label>Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.update('password')}
+            />
+          </label>
+          <br />
+          <input type="submit" value={this.props.formType} />
+        </form>
+      </div>
+    )
+  }
+}
+
+SessionForm.propTypes = {
+  errors: PropTypes.array.isRequired,
+  processForm: PropTypes.func.isRequired,
+  formType: PropTypes.string.isRequired
+}
+
+export default SessionForm;
