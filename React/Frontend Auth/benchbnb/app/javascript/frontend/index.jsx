1db834912a0e96c96256418d3d3055ad61b92103
Matt Ramos <mattramos@gmail.com>
2020-02-16 11:24:03 -0500

    Add Store

--- React/Frontend Auth/benchbnb/app/javascript/frontend/index.jsx
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
