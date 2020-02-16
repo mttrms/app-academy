36e15629fcd0893dbdba25b884eb7a8bfb741a36
Matt Ramos <mattramos@gmail.com>
2020-02-16 13:07:34 -0500

    Add Greeting container

---
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/greeting_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Greeting from './greeting';
+import { logout } from '../actions/session_actions';
+
+const mapStateToProps = state => ({
+  currentUser: state.entities.users[state.session.id]
+});
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Greeting);
