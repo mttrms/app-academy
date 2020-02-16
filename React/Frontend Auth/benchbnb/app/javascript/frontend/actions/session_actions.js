9f7575cba9a1743abddb7a0b068ce578fbf4d2e5
Matt Ramos <mattramos@gmail.com>
2020-02-15 22:40:52 -0500

    Add action creators

--- React/Frontend Auth/benchbnb/app/javascript/frontend/actions/session_actions.js
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/actions/session_actions.js
@@ -3,3 +3,17 @@ import * as APIUtil from '../util/session_api_util';
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
