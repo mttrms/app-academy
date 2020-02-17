a4eee0f8ab1ca772573c081fb6f6802541b122b5
Matt Ramos <mattramos@gmail.com>
2020-02-17 00:48:37 -0500

    Add Marker Manager for map

---
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/marker_manager.js
@@ -0,0 +1,10 @@
+export default class MarkerManager {
+  constructor(map) {
+    this.map = map;
+    this.markers = {};
+  }
+
+  updateMarkers(benches) {
+    console.log('updates to come');
+  }
+}
