fd7f5e7907a6bd4bb643949a9fa9624f10462790
Matt Ramos <mattramos@gmail.com>
2020-02-16 19:51:14 -0500

    Add AuthRoute

--- React/Frontend Auth/benchbnb/app/javascript/frontend/components/app.jsx
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
@@ -12,8 +13,8 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path='/login' component={LoginFormContainer} />
-    <Route path='/signup' component={SignupFormContainer} />
+    <AuthRoute path='/login' component={LoginFormContainer} />
+    <AuthRoute path='/signup' component={SignupFormContainer} />
   </div>
 );
 
