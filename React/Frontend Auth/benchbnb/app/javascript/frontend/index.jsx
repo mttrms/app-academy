85075731e8b4c340aac9f7074088b600ac409565
Matt Ramos <mattramos@gmail.com>
2020-02-16 19:32:13 -0500

    Set preloaded state for logged in users

--- React/Frontend Auth/benchbnb/app/javascript/frontend/index.jsx
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/index.jsx
@@ -6,7 +6,20 @@ import { login } from './actions/session_actions';
 import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={ store } />, root);
 
