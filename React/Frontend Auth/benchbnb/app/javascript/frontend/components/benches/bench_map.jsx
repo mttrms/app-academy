927c05625af10a003f6d8fc3604d706ec9882d3f
Matt Ramos <mattramos@gmail.com>
2020-02-17 00:09:33 -0500

    Actually render a map

--- React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/bench_map.jsx
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/bench_map.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 class BenchMap extends React.Component {
   constructor(props) {
     super(props);
-    this.mapRef = React.createRef()
+  }
+
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 40.720135, lng: -74.042943 },
+      zoom: 15 
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
   }
 
   render() {
-    return <div id="map-container" ref={this.mapRef}></div>
+    return <div id="map-container" ref={ map => this.mapNode = map }></div>
   }
 }
 
