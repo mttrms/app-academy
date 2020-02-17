0788e43a4a2b672ba9d8775f1b2aa6a8715d3729
Matt Ramos <mattramos@gmail.com>
2020-02-16 21:50:07 -0500

    Add Bench model, migration + routes

--- React/Frontend Auth/benchbnb/db/schema.rb
+++ React/Frontend Auth/benchbnb/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_052822) do
+ActiveRecord::Schema.define(version: 2020_02_17_024629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "benches", force: :cascade do |t|
+    t.string "description", null: false
+    t.float "lat", null: false
+    t.float "lng", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
