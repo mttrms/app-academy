91343c7e11ba953bd1fb5aa1cf71d37c166cda8b
Matt Ramos <mattramos@gmail.com>
2020-02-17 21:11:00 -0500

    Add bench markers to map

--- React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/bench_map.jsx
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/bench_map.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MarkerManager from './marker_manager';
+
 class BenchMap extends React.Component {
   constructor(props) {
     super(props);
@@ -12,6 +14,12 @@ class BenchMap extends React.Component {
     };
 
     this.map = new google.maps.Map(this.mapNode, mapOptions);
+    this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager.updateMarkers(this.props.benches);
+  }
+
+  componentDidUpdate() {
+    this.MarkerManager.updateMarkers(this.props.benches);
   }
 
   render() {
--- React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/marker_manager.js
+++ React/Frontend Auth/benchbnb/app/javascript/frontend/components/benches/marker_manager.js
@@ -2,9 +2,24 @@ export default class MarkerManager {
   constructor(map) {
     this.map = map;
     this.markers = {};
+    console.log(this.map);
   }
 
   updateMarkers(benches) {
-    console.log('updates to come');
+    benches
+      .filter(bench => !this.markers[bench.id])
+      .forEach(newBench => this.createMarkerFromBench(newBench))
+  }
+
+  createMarkerFromBench(bench) {
+    const position = new google.maps.LatLng(bench.lat, bench.lng);
+    const marker = new google.maps.Marker({
+      position,
+      map: this.map,
+      benchId: bench.id
+    });
+
+    this.markers[marker.benchId] = marker;
+    console.log(this.markers)
   }
 }
